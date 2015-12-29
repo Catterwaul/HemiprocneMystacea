eb5789bdbec011165be626e800363162fd3b04cd
Jessy Catterwaul <jessy@ceilink.com>
2015-12-29 15:52:01 -0500

    Initial Commit

---
+++ HemiprocneMystacea/HemiprocneMystacea.h
@@ -0,0 +1,19 @@
+//
+//  HemiprocneMystacea.h
+//  HemiprocneMystacea
+//
+//  Created by Jessy Catterwaul on 12/29/15.
+//  Copyright © 2015 Catterwaul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HemiprocneMystacea.
+FOUNDATION_EXPORT double HemiprocneMystaceaVersionNumber;
+
+//! Project version string for HemiprocneMystacea.
+FOUNDATION_EXPORT const unsigned char HemiprocneMystaceaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HemiprocneMystacea/PublicHeader.h>
+
+
