b7eeb5e2aea06b4b58389804a16db9fd22f41eb5
Jessy Catterwaul <mr.jessy@gmail.com>
2015-12-30 23:48:00 -0500

    Add 5 and 6 OptionSetType.Flags

--- HemiprocneMystacea/Swift/Extensions/OptionSetType.swift
+++ HemiprocneMystacea/Swift/Extensions/OptionSetType.swift
@@ -35,6 +35,35 @@ public extension OptionSetType where RawValue: BitShiftable {
       )
    }
    
+   /// Provides five options.
+   ///
+   ///- Parameter startingIndex: shifts 0b1_1111 if > 1
+   static func Flags(startingIndex startingIndex: RawValue = 1)
+   -> (Self, Self, Self, Self, Self) {
+      return (
+         Self(flagIndex: startingIndex),
+         Self(flagIndex: startingIndex + 1),
+         Self(flagIndex: startingIndex + 2),
+         Self(flagIndex: startingIndex + 3),
+         Self(flagIndex: startingIndex + 4)
+      )
+   }
+   
+   /// Provides six options.
+   ///
+   ///- Parameter startingIndex: shifts 0b11_1111 if > 1
+   static func Flags(startingIndex startingIndex: RawValue = 1)
+   -> (Self, Self, Self, Self, Self, Self) {
+      return (
+         Self(flagIndex: startingIndex),
+         Self(flagIndex: startingIndex + 1),
+         Self(flagIndex: startingIndex + 2),
+         Self(flagIndex: startingIndex + 3),
+         Self(flagIndex: startingIndex + 4),
+         Self(flagIndex: startingIndex + 5)
+      )
+   }
+   
    private init(flagIndex: RawValue) {
       self.init(
          rawValue: flagIndex <= 1 ? flagIndex : 1 << (flagIndex - 1)
--- HemiprocneMystaceaTests/Swift/Extensions/OptionSetTypeTestCase.swift
+++ HemiprocneMystaceaTests/Swift/Extensions/OptionSetTypeTestCase.swift
@@ -16,6 +16,17 @@ final class OptionSetTypeTestCase: XCTestCase {
       let options: Options = [.Option6, .Option7, .Option8, .Option9]
       XCTAssertEqual(options.rawValue, 0b1_1110_0000)
    }
+   
+   func test5Flags() {
+      let options: Options
+      = [.Option10, .Option11, .Option12, .Option13, .Option14]
+      XCTAssertEqual(options.rawValue, 0b11_1110_0000_0000)
+   }
+   
+   func test6Flags() {
+      let options: Options
+      = [.Option15, .Option16, .Option17, .Option18, .Option19, .Option20]
+      XCTAssertEqual(options.rawValue, 0b1111_1100_0000_0000_0000)   }
 }
 
 private struct Options: OptionSetType {
@@ -25,5 +36,8 @@ private struct Options: OptionSetType {
    static let
       (Option1, Option2) = Options.Flags(),
       (Option3, Option4, Option5) = Options.Flags(startingIndex: 3),
-      (Option6, Option7, Option8, Option9) = Options.Flags(startingIndex: 6)
+      (Option6, Option7, Option8, Option9) = Options.Flags(startingIndex: 6),
+      (Option10, Option11, Option12, Option13, Option14) = Options.Flags(startingIndex: 10),
+      (Option15, Option16, Option17, Option18, Option19, Option20)
+      = Options.Flags(startingIndex: 15)
 }
