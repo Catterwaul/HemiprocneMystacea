5736c0671f0233c8f36501a327185b6a952df754
Jessy Catterwaul <mr.jessy@gmail.com>
2016-01-02 01:36:14 -0500

    nils

--- HemiprocneMystacea/Swift/nil closures.swift
+++ HemiprocneMystacea/Swift/nil closures.swift
@@ -38,3 +38,44 @@ public func nillifyUponCall<In, Out>(
    }
    closure = nilledUponCall(closure, closure_set)
 }
+
+//MARK:- nils
+
+///- Returns: Two generic closures which return nil
+public func Nils<
+   Arguments1, Return1,
+   Arguments2, Return2
+>() -> (
+   Arguments1 -> Return1?,
+   Arguments2 -> Return2?
+) {
+   return ({_ in nil}, {_ in nil})
+}
+
+///- Returns: - Returns: Three generic closures which return nil
+public func Nils<
+   Arguments1, Return1,
+   Arguments2, Return2,
+   Arguments3, Return3
+>() -> (
+   Arguments1 -> Return1?,
+   Arguments2 -> Return2?,
+   Arguments3 -> Return3?
+) {
+   return ({_ in nil}, {_ in nil}, {_ in nil})
+}
+
+///- Returns: - Returns: Four generic closures which return nil
+public func Nils<
+   Arguments1, Return1,
+   Arguments2, Return2,
+   Arguments3, Return3,
+   Arguments4, Return4
+>() -> (
+   Arguments1 -> Return1?,
+   Arguments2 -> Return2?,
+   Arguments3 -> Return3?,
+   Arguments4 -> Return4?
+) {
+   return ({_ in nil}, {_ in nil}, {_ in nil}, {_ in nil})
+}
