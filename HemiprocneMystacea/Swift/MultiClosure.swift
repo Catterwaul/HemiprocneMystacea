462e3a6e9fda01a7c67242e242dc46236d3bf8e0
Jessy Catterwaul <mr.jessy@gmail.com>
2016-01-02 18:17:27 -0500

    MultiClosure & EquatableClosure

--- HemiprocneMystacea/Swift/MultiClosure.swift
+++ HemiprocneMystacea/Swift/MultiClosure.swift
@@ -1,55 +1,90 @@
+/// A workaround for Swift not providing a way to remove closures
+/// from a collection of closures.
+///- Note: Designed for one-to-many events, hence no return value.
+///  Returning a single value from multiple closures doesn't make sense.
 public final class MultiClosure<Input>: EquatableClass {
-	/// We can't override () so maybe brackets are the next best thing?
-	public subscript(input: Input) -> () {
-		closures.forEach{$0.reference?[input]}
-	}
-   
    public init(_ closures: EquatableClosure<Input>...) {self += closures}
    
-	private var closures = Set<
-		WeakReferencer<EquatableClosure<Input>>
-	>()
-	
-	deinit {
-		closures.forEach{$0.reference?.multiClosures -= self}
-	}
+   public init<Closures: SequenceType
+      where Closures.Generator.Element == EquatableClosure<Input>
+   >(_ closures: Closures) {self += closures}
+   
+   /// Execute every closure
+   ///
+   /// ***Rationale:***
+   /// We can't override () so maybe brackets are the next best thing?
+   public subscript(input: Input) -> () {
+      closures.forEach{$0.reference[input]}
+   }
+   
+   var closures = Set<
+      UnownedReferencer<EquatableClosure<Input>>
+   >()
+   
+//MARK:- Deallocation
+   // We can't find self in `closures` without this.
+   private lazy var unownedSelf: UnownedReferencer<MultiClosure<Input>>
+   = UnownedReferencer(self)
+   
+   // Even though this MultiClosure will be deallocated,
+   // its corresponding WeakReferencers won't be,
+   // unless we take this manual action or similar.
+   deinit {
+      closures.forEach{$0.reference.multiClosures -= unownedSelf}
+   }
 }
 
+/// A wrapper around a closure, for use with MultiClosures
 public final class EquatableClosure<Input>: EquatableClass {
 	public init(_ closure: Input -> ()) {self.closure = closure}
 
+   /// Execute the closure
+   ///
+   /// ***Rationale:***
+   /// We can't override () so maybe brackets are the next best thing?
 	public subscript(input: Input) -> () {closure(input)}
 
 	private let closure: Input -> ()
 
-	private var multiClosures = Set<
-		WeakReferencer<MultiClosure<Input>>
-	>()
-	
+//MARK:- Deallocation
+   var multiClosures = Set<
+      UnownedReferencer<MultiClosure<Input>>
+   >()
+   
+   // We can't find self in `multiClosures` without this.
+   private lazy var unownedSelf: UnownedReferencer<EquatableClosure<Input>>
+   = UnownedReferencer(self)
+   
 	deinit {
-		multiClosures.forEach{$0.reference?.closures -= self}
+ 		multiClosures.forEach{$0.reference.closures -= unownedSelf}
 	}
 }
 
+/// Add `closure` to the set of closures that runs
+/// when `multiClosure` does
 public func += <Input>(
    multiClosure: MultiClosure<Input>,
    closure: EquatableClosure<Input>
 ) {
-   multiClosure.closures += WeakReferencer(closure)
-   closure.multiClosures += WeakReferencer(multiClosure)
+   multiClosure.closures += closure.unownedSelf
+   closure.multiClosures += multiClosure.unownedSelf
 }
 
-public func += <Input>(
-   multiClosure: MultiClosure<Input>,
-   closures: [EquatableClosure<Input>]
-) {
+/// Add `closures` to the set of closures that runs
+/// when `multiClosure` does
+public func += <
+   Input,
+   Closures: SequenceType where Closures.Generator.Element == EquatableClosure<Input>
+>(multiClosure: MultiClosure<Input>, closures: Closures) {
    for closure in closures {multiClosure += closure}
 }
 
+/// Remove `closure` from the set of closures that runs
+/// when `multiClosure` does
 public func -= <Input>(
    multiClosure: MultiClosure<Input>,
    closure: EquatableClosure<Input>
 ) {
-	multiClosure.closures -= closure
-	closure.multiClosures -= multiClosure
+	multiClosure.closures -= closure.unownedSelf
+   closure.multiClosures -= multiClosure.unownedSelf
 }
---
+++ HemiprocneMystaceaTests/Swift/MultiClosureTestCase.swift
@@ -0,0 +1,45 @@
+@testable import HemiprocneMystacea
+import XCTest
+
+final class MultiClosureTestCase: XCTestCase {
+   func testMultiClosure() {
+      var x = 0
+      let closures = [
+         EquatableClosure{x += 1},
+         EquatableClosure{x += 2}
+      ]
+      let multiClosure = MultiClosure(closures)
+      multiClosure[]
+      XCTAssertEqual(x, 3)
+      multiClosure -= closures.first!
+      multiClosure[]
+      XCTAssertEqual(x, 5)
+   }
+   
+   func testSetRemoval() {
+      let
+         closure: EquatableClosure<()>! = EquatableClosure{},
+         multiClosure = MultiClosure(closure)
+      XCTAssertNotEqual(multiClosure.closures, [])
+      XCTAssertNotEqual(closure.multiClosures, [])
+      multiClosure -= closure
+      XCTAssertEqual(multiClosure.closures, [])
+      XCTAssertEqual(closure.multiClosures, [])
+   }
+
+   func testDeallocation() {
+      var
+         closure: EquatableClosure<()>! = EquatableClosure{},
+         multiClosure: MultiClosure<()>!  = MultiClosure(closure)
+      
+      XCTAssertNotEqual(multiClosure.closures, [])
+      closure = nil
+      XCTAssertEqual(multiClosure.closures, [])
+      
+      closure = EquatableClosure{}
+      multiClosure += closure
+      XCTAssertNotEqual(closure.multiClosures, [])
+      multiClosure = nil
+      XCTAssertEqual(closure.multiClosures, [])
+   }
+}
