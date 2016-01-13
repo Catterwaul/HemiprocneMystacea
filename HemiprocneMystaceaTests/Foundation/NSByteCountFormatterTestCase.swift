2cebb9dac87860fed801e3870d6706df56ef60a6
Jessy Catterwaul <mr.jessy@gmail.com>
2016-01-13 18:02:54 -0500

    NSByteCountFormatter

---
+++ HemiprocneMystaceaTests/Foundation/NSByteCountFormatterTestCase.swift
@@ -0,0 +1,20 @@
+import HemiprocneMystacea
+import XCTest
+
+final class NSByteCountFormatterTestCaseTestCase: XCTestCase {
+   func testString() {
+      let formatter = NSByteCountFormatter(includesUnit: true)
+      XCTAssertEqual(
+         [
+            100,
+            6565,
+            4_000_000
+         ].map{formatter.String($0)},
+         [
+            "100 bytes",
+            "7 KB",
+            "4 MB"
+         ]
+      )
+   }
+}
