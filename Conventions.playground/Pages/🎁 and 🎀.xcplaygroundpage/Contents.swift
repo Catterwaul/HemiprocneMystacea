45333d88ec7fd0f24b36b14ab1749e1a17a2011d
Catie Catterwaul <catiet@gmail.com>
2016-01-14 21:38:14 -0500

    🎁 and 🎀

---
+++ Conventions.playground/Pages/🎁 and 🎀.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+/*:
+## 🎁: decoration that indicates that something returns a value, that might be nil or empty
+## 🎀: same thing, except it’s never nil or empty
+
+### Examples can be found in:
+* Int🎁.swift
+* String🎁.swift
+
+* Double🎀.swift
+*/
