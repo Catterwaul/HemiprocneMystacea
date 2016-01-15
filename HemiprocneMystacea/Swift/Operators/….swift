12735a9c4ff8456a93bf86b4a65983d9701db6d4
Jessy Catterwaul <mr.jessy@gmail.com>
2016-01-14 22:57:13 -0500

    Recursion …

--- HemiprocneMystacea/Swift/Operators/….swift
+++ HemiprocneMystacea/Swift/Operators/….swift
@@ -16,10 +16,21 @@ infix operator … {precedence 255}
 /// }
 ///```
 ///- Remark: Hold option, press ;
-public func …<Instance>(
-   instance: Instance,
-   @noescape ƒ: Instance -> ()
-) -> Instance {
+public func …<Type>(
+   instance: Type,
+   @noescape ƒ: Type -> ()
+) -> Type {
    ƒ(instance)
    return instance
+}
+
+// This results in a Segmentation fault; 
+// use it when that's no longer the case.
+//public func …<Type>(instances: Type..., recursed: Type -> [Type]) -> [Type]  {
+//   return (instances)…(recursed)
+//}
+
+/// Recursively get `Type`s via the `recursed` function
+public func …<Type>(instances: [Type], recursed: Type -> [Type]) -> [Type]  {
+   return instances.flatMap{[$0] + recursed($0)…recursed}
 }
--- HemiprocneMystaceaTests/Swift/OperatorsTestCase.swift
+++ HemiprocneMystaceaTests/Swift/OperatorsTestCase.swift
@@ -7,7 +7,7 @@ final class OperatorsTestCase: XCTestCase {
       XCTAssertEqual(22•string, "22")
    }
    
-   func testEllipsis() {
+   func testInitializationEllipsis() {
       class Class {
          var bool: Bool!
       }
@@ -15,5 +15,38 @@ final class OperatorsTestCase: XCTestCase {
          $0.bool = true
       }
       XCTAssertEqual(`class`.bool, true)
+   }
+
+   func testRecursionEllipsis() {
+      struct Branch {
+         let datum: Int
+         let branches: [Branch]
+      }
+      
+      let branch1 = Branch(datum: 1, branches: [])
+      let branch2 = Branch(datum: 2, branches: [branch1])
+      let branch3 = Branch(datum: 3, branches: [branch2, branch1])
+      let branch4 = Branch(datum: 4, branches: [branch3, branch2, branch1])
+      
+      XCTAssertEqual(
+         ([branch1, branch2, branch3, branch4]…{$0.branches}).map{$0.datum},
+         [
+            1,
+            2,
+               1,
+            3,
+               2,
+                  1,
+               1,
+            4,
+               3,
+                  2,
+                     1,
+                  1,
+               2,
+                  1,
+               1
+         ]
+      )
    }
 }
