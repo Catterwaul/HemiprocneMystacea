87ea2972e605adf5962f1f06d2c13fc4294bf0e2
Jessy Catterwaul <mr.jessy@gmail.com>
2016-02-07 12:48:52 -0500

    Add usage of generic arguments

--- Conventions/Conventions.playground/Pages/Functions, no return value.xcplaygroundpage/Contents.swift
+++ Conventions/Conventions.playground/Pages/Functions, no return value.xcplaygroundpage/Contents.swift
@@ -16,20 +16,21 @@ Only the verb remains outside the parentheses.
 */
 func write(sentence sentence: String) {}
 /*:
-Sometimes giving a direct object a parameter name does not add meaning. Frequently, this will be the case with generic functions with no, or very nonrestrictive, constraints. Closures currently utilize `$0` for this, but Swift doesn't yet allow us to omit parameter names in functions, and we can't begin parameter names with $. Use `_0` instead of `_$0`, because closures should use the term `.0`, instead of `$0`. Assume that as Swift improves, your underscores will eventually becoming dots, or at least dollar signs, with no correspongding manual paramter name.
+Sometimes giving a direct object a parameter name does not add meaning. Frequently, this will be the case with generic functions with no, or very nonrestrictive, constraints. Closures currently utilize `$0` for this, but Swift doesn't yet allow us to omit parameter names in functions, and we can't begin parameter names with $. Use `_0` instead of `_$0`, because closures should use the term `.0`, instead of `$0`. Assume that as Swift improves, your underscores will eventually becoming dots, or at least dollar signs, with no correspongding manual parameter name.
 */
-func write<🃏>(_0: 🃏) {}
+func write<🃏>(_0: 🃏) {print(_0)}
+write("✍️")
 
 /// Future Swift?
-// func write($) {}
+// func write($) {print(.0)}
 /*:
 Indirect objects need external parameter names, but not necessarily internal ones. The external parameter names will  either be…
-### …Prepositions
+### …Prepositions…
 */
 func write(to buddy: AnyObject) {}
 func write<🃏>(to _0: 🃏) {}
 /*:
-### …Prepositions and their objects.
+### …or Prepositions and their objects.
 When this happens, the corresponding interal parameter name will be the same, but with the preposition removed.
 */
 func write(toBuddy buddy: AnyObject) {}
