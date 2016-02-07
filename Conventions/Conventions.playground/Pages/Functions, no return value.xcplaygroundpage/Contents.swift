e568ac934de6a00031a810f94df601540a8abaaa
Jessy Catterwaul <mr.jessy@gmail.com>
2016-02-07 12:34:34 -0500

    Functions, no return value

---
+++ Conventions/Conventions.playground/Pages/Functions, no return value.xcplaygroundpage/Contents.swift
@@ -0,0 +1,35 @@
+/*:
+# Functions that do not return values can be…
+## …Verbs
+Verbs do not take arguments.
+*/
+func write() {}
+/*:
+## …Predicates
+A verb and its direct object will both be outside an empty parameter list, when that object does not need to vary, and therefore not be passed as an argument. Articles (such as the "a" in "a sentence") are not used.
+*/
+func writeSentence() {}
+/*:
+When the direct object does need to be an argument, it will have the same parameter name internally and externally. Currently, Swift uses the wrong default for this, so we must type the name twice.
+
+Only the verb remains outside the parentheses.
+*/
+func write(sentence sentence: String) {}
+/*:
+Sometimes giving a direct object a parameter name does not add meaning. Frequently, this will be the case with generic functions with no, or very nonrestrictive, constraints. Closures currently utilize `$0` for this, but Swift doesn't yet allow us to omit parameter names in functions, and we can't begin parameter names with $. Use `_0` instead of `_$0`, because closures should use the term `.0`, instead of `$0`. Assume that as Swift improves, your underscores will eventually becoming dots, or at least dollar signs, with no correspongding manual paramter name.
+*/
+func write<🃏>(_0: 🃏) {}
+
+/// Future Swift?
+// func write($) {}
+/*:
+Indirect objects need external parameter names, but not necessarily internal ones. The external parameter names will  either be…
+### …Prepositions
+*/
+func write(to buddy: AnyObject) {}
+func write<🃏>(to _0: 🃏) {}
+/*:
+### …Prepositions and their objects.
+When this happens, the corresponding interal parameter name will be the same, but with the preposition removed.
+*/
+func write(toBuddy buddy: AnyObject) {}
