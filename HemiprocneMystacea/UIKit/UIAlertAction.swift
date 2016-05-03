31dd3525bb5f6d4799e41de7cb38b4a8ff1970fc
Jessy Catterwaul <mr.jessy@gmail.com>
2016-05-03 17:02:28 -0400

    UIAlertAction convenience init

--- HemiprocneMystacea/UIKit/UIAlertAction.swift
+++ HemiprocneMystacea/UIKit/UIAlertAction.swift
@@ -1,7 +1,17 @@
 public extension UIAlertAction {
-	@nonobjc static let `default` = UIAlertAction(
-		title: "OK",
-		style: .Default,
-		handler: nil
-	)
+	@nonobjc static let `default` = UIAlertAction(title: "OK")
+	
+	convenience init(
+		title: String? = nil,
+		style: UIAlertActionStyle = .Default,
+		📻selection: () -> () = {}
+	) {
+		self.init(
+			title: title,
+			style: style,
+			handler: {_ in
+				📻selection()
+			}
+		)
+	}
 }
