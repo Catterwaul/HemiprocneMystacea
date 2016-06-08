732fbe75564a1cfb045529ac822e9ae96dbbd940
Jessy Catterwaul <jessy.catterwaul@amtrustgroup.com>
2016-06-08 15:13:19 -0400

    Clean up FileInfo

--- HemiprocneMystacea/Foundation/FileInfo.swift
+++ HemiprocneMystacea/Foundation/FileInfo.swift
@@ -2,8 +2,14 @@ struct FileInfo {
 	init(path: String) throws {
 		try attributes = NSFileManager.defaultManager().attributesOfItemAtPath(path)
 	}
-	
+
 	private let attributes: [String: AnyObject]
-	
-	var size: UInt {return attributes[NSFileSize] as! UInt}
+}
+
+//MARK: public
+extension FileInfo {
+
+	var size: UInt {
+		return attributes[NSFileSize] as! UInt
+	}
 }
