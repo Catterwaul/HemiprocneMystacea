693001f46c877f247dabb7a71467c5e4746efdeb
Jessy Catterwaul <jessy.catterwaul@amtrustgroup.com>
2016-06-08 14:57:55 -0400

    Clean up Contacts

--- HemiprocneMystacea/Contacts/CNContainer.swift
+++ HemiprocneMystacea/Contacts/CNContainer.swift
@@ -1,5 +1,8 @@
 import Contacts
 
 extension CNContainer {
-   var isICloud: Bool {return type == .CardDAV && name == "Card"}
+	var isICloud: Bool {
+		return type == .CardDAV
+			&& name == "Card"
+	}
 }
