77cb4c6810f1905b7daf15057c77b8d874e94939
Jessy Catterwaul <mr.jessy@gmail.com>
2016-06-08 23:27:38 -0400

    OptionalTestCase

---
+++ HemiprocneMystaceaTests/Swift/Extensions/OptionalTestCase.swift
@@ -0,0 +1,10 @@
+import HemiprocneMystacea
+import XCTest
+
+final class OptionalTestCase: XCTestCase {
+  func testAssignedIfNil() {
+    var bool: Bool? = nil
+    XCTAssertTrue(bool.assignedIfNil{true})
+    XCTAssertTrue(bool.assignedIfNil{false})
+  }
+}
