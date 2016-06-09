0f4a92d225012afcbe162f5a065912c3e848f273
Jessy Catterwaul <mr.jessy@gmail.com>
2016-06-08 23:34:08 -0400

    NSUserDefaults formatting

--- HemiprocneMystacea/Foundation/NSUserDefaults+Setting.swift
+++ HemiprocneMystacea/Foundation/NSUserDefaults+Setting.swift
@@ -1,4 +1,6 @@
-public struct Setting<Value: AnyObject> {
+public struct Setting
+<Value: AnyObject>
+{
 	public init(key: String, defaultValue: Value) {
 		self.key = key
 		NSUserDefaults.standardUserDefaults().registerDefaults(
@@ -12,16 +14,19 @@ public struct Setting<Value: AnyObject> {
 	///- Returns: **value**
 	///- Parameter getter: The method of `NSUserDefaults` that returns a **`Value`**,
 	///  when supplied with a key that is a `String`
-	private func value<Value>(
+	private func value
+  <Value>(
       getter value: NSUserDefaults -> (`for`: String) -> Value
-   ) -> Value {
+  ) -> Value {
 		return (NSUserDefaults.standardUserDefaults()•value)(for: key)
 	}
    
    ///- Returns: The setter for **value**
 	///- Parameter setter: The method of `NSUserDefaults` that writes a **`Value`**,
 	///  for a key that is a `String`
-	private func value_set<Value>(value: Value,
+	private func value_set
+  <Value>(
+    value: Value,
 		setter set: NSUserDefaults -> (Value, `for`: String) -> ()
 	) {
 		(NSUserDefaults.standardUserDefaults()•set)(value, for: self.key)
@@ -30,14 +35,22 @@ public struct Setting<Value: AnyObject> {
 
 extension Setting where Value: BooleanLiteralConvertible {
 	public var value: Bool {
-		get {return value(getter: NSUserDefaults.boolForKey)}
-		set {value_set(newValue, setter: NSUserDefaults.setBool)}
+		get {
+      return value(getter: NSUserDefaults.boolForKey)
+    }
+		set {
+      value_set(newValue, setter: NSUserDefaults.setBool)
+    }
 	}
 }
 
 extension Setting where Value: IntegerLiteralConvertible {
 	public var value: Int {
-		get {return value(getter: NSUserDefaults.integerForKey)}
-		set {value_set(newValue, setter: NSUserDefaults.setInteger)}
+		get {
+      return value(getter: NSUserDefaults.integerForKey)
+    }
+		set {
+      value_set(newValue, setter: NSUserDefaults.setInteger)
+    }
 	}
 }
