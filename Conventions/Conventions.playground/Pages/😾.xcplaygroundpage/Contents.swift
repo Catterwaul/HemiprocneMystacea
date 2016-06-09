21006dad981ed44ac5145c3434c4dc3d0ce0f7d0
Catie Catterwaul <catiet@gmail.com>
2016-06-08 22:07:59 -0400

    Delete 😾

--- Conventions/Conventions.playground/Pages/😾.xcplaygroundpage/Contents.swift
+++
@@ -1,7 +0,0 @@
-/*:
-## Use a 😾 in front of something when…
-1. you think it's wrong that you have to use that thing.
-2. aside from the 😾, it has the same name as the thing you actually want to use.
-### Examples can be found in:
-* dequeuedReusableCell.swift
-*/
