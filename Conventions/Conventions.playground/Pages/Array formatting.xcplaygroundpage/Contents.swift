c5f03afea22c45c7a37c02bc34d64f2ae4b1e864
Catie Catterwaul <catiet@gmail.com>
2016-06-08 22:38:31 -0400

    Array Formatting

---
+++ Conventions/Conventions.playground/Pages/Array formatting.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+/*:
+# Array Formatting
+You may have one or two elements on one line in an array.
+*/
+let thunderCats = ["Lion-o"]
+
+let thunderThunderCats = ["Panthro", "Cheetara"]
+
+//: For three or more elements, use multiple lines.
+let thunderThunderThunderCats_Hoooooo = [
+	"Jaga the Wise",
+	"Tygra",
+	"WilyKit",
+	"WilyKat"
+]
