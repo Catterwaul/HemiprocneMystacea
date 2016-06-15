c404ee4a9633f5627947b3bd598a8a53c80f50e9
Jessy Catterwaul <mr.jessy@gmail.com>
2016-06-15 00:37:47 -0400

    UIButton

--- HemiprocneMystacea/UIKit/UIButton.swift
+++ HemiprocneMystacea/UIKit/UIButton.swift
@@ -1,10 +1,12 @@
 public extension UIButton {
-   /// A simple way to visibily show that a user interaction is disabled
-   final var disabledAndDim: Bool {
-      get {return !enabled}
-      set {
-         enabled = !newValue
-         alpha = newValue ? 0.5 : 1
-      }
-   }
+	/// A simple way to visibily show that a user interaction is disabled
+	final var disabledAndDim: Bool {
+		get {return !isEnabled}
+		set {
+			isEnabled = !newValue
+			alpha = newValue
+				? 0.5
+				: 1
+		}
+	}
 }
