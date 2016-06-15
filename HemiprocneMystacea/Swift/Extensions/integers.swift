4c92d3a4657ac236b2ef3aa0a9bed2ec418a6f42
Jessy Catterwaul <mr.jessy@gmail.com>
2016-06-15 00:08:23 -0400

    integers

--- HemiprocneMystacea/Swift/Extensions/integers.swift
+++ HemiprocneMystacea/Swift/Extensions/integers.swift
@@ -4,3 +4,12 @@ public extension IntegerLiteralConvertible where Self: IntegerArithmetic {
 		return self % divisor == 0
 	}
 }
+
+extension Sequence where Iterator.Element: protocol<
+	IntegerLiteralConvertible,
+	IntegerArithmetic
+> {
+	public var sum: Iterator.Element {
+		return self.reduce(0, combine: +)
+	}
+}
