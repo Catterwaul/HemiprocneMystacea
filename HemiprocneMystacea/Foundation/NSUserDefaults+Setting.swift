cb7ba75a1e2baaca48b44da48eed8004ae6f5e9f
Jessy Catterwaul <mr.jessy@gmail.com>
2016-06-14 23:02:54 -0400

    NSUserDefaults+Setting

--- HemiprocneMystacea/Foundation/NSUserDefaults+Setting.swift
+++ HemiprocneMystacea/Foundation/NSUserDefaults+Setting.swift
@@ -1,56 +1,71 @@
 public struct Setting
 <Value: AnyObject>
 {
-	public init(key: String, defaultValue: Value) {
+	public init(
+		key: String,
+		defaultValue: Value
+	) {
 		self.key = key
-		NSUserDefaults.standardUserDefaults().registerDefaults(
+		UserDefaults.standard().register(
 			[key: defaultValue]
 		)
-		NSUserDefaults.standardUserDefaults().synchronize()
+		UserDefaults.standard().synchronize()
 	}
 	
 	private let key: String
-	
+}
+
+//MARK: BooleanLiteralConvertible
+public extension Setting where Value: BooleanLiteralConvertible {
+	var value: Bool {
+		get {
+      return value(getter: UserDefaults.bool(forKey:))
+    }
+		set {
+      value_set(
+				newValue,
+				setter: UserDefaults.set
+			)
+    }
+	}
+}
+
+//MARK: IntegerLiteralConvertible
+public extension Setting where Value: IntegerLiteralConvertible {
+	var value: Int {
+		get {
+      return value(getter: UserDefaults.integer(forKey:))
+    }
+		set {
+      value_set(
+				newValue,
+				setter: UserDefaults.set
+			)
+    }
+	}
+}
+
+
+//MARK: private 
+private extension Setting {
 	///- Returns: **value**
 	///- Parameter getter: The method of `NSUserDefaults` that returns a **`Value`**,
 	///  when supplied with a key that is a `String`
-	private func value
+	func value
   <Value>(
-      getter value: NSUserDefaults -> (`for`: String) -> Value
+      getter value: (UserDefaults) -> (for: String) -> Value
   ) -> Value {
-		return (NSUserDefaults.standardUserDefaults()…value)(for: key)
+		return (UserDefaults.standard()…value)(for: key)
 	}
    
    ///- Returns: The setter for **value**
 	///- Parameter setter: The method of `NSUserDefaults` that writes a **`Value`**,
 	///  for a key that is a `String`
-	private func value_set
+	func value_set
   <Value>(
-    value: Value,
-		setter set: NSUserDefaults -> (Value, `for`: String) -> ()
+    _ value: Value,
+		setter set: (UserDefaults) -> (Value, for: String) -> ()
 	) {
-		(NSUserDefaults.standardUserDefaults()…set)(value, for: self.key)
-	}
-}
-
-extension Setting where Value: BooleanLiteralConvertible {
-	public var value: Bool {
-		get {
-      return value(getter: NSUserDefaults.boolForKey)
-    }
-		set {
-      value_set(newValue, setter: NSUserDefaults.setBool)
-    }
-	}
-}
-
-extension Setting where Value: IntegerLiteralConvertible {
-	public var value: Int {
-		get {
-      return value(getter: NSUserDefaults.integerForKey)
-    }
-		set {
-      value_set(newValue, setter: NSUserDefaults.setInteger)
-    }
+		(UserDefaults.standard()…set)(value, for: self.key)
 	}
 }
