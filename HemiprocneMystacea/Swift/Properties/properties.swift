f5341fe83fe593b443613a7381631eec86235940
Jessy Catterwaul <mr.jessy@gmail.com>
2016-06-15 00:19:57 -0400

    properties

--- HemiprocneMystacea/Swift/Properties/properties.swift
+++ HemiprocneMystacea/Swift/Properties/properties.swift
@@ -2,6 +2,6 @@
 /// until Swift has real set-only properties.
 ///- Important: Causes a fatal error, logging the name of the property.
 ///- Parameter propertyName: Never use this; only use the default.
-@noreturn public func setOnlyPropertyGetterError(propertyName: String = #function) {
+@noreturn public func setOnlyPropertyGetterError(_ propertyName: String = #function) {
    fatalError("\(propertyName) is set-only")
 }
