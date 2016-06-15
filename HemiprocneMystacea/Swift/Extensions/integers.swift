c4cf7d1f79adb199510287eb9f0ab686d96f9f16
Jessy Catterwaul <mr.jessy@gmail.com>
2016-06-14 23:49:07 -0400

    integers

--- HemiprocneMystacea/Swift/Extensions/integers.swift
+++ HemiprocneMystacea/Swift/Extensions/integers.swift
@@ -1,4 +1,4 @@
-public extension IntegerLiteralConvertible where Self: IntegerArithmeticType {
+public extension IntegerLiteralConvertible where Self: IntegerArithmetic {
 	/// *Divisbility*: when division by `divisor` results in a whole number.
 	func divisible(by divisor: Self) -> Bool {
 		return self % divisor == 0
