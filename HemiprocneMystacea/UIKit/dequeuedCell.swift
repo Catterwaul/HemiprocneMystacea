1e9678ad2788e622f72eb9987a8bc45d87a32d66
Jessy Catterwaul <mr.jessy@gmail.com>
2016-06-16 20:51:30 -0400

    dequeuedCell

--- HemiprocneMystacea/UIKit/dequeuedCell.swift
+++ HemiprocneMystacea/UIKit/dequeuedCell.swift
@@ -1,20 +1,20 @@
-//MARK: cell
+//MARK: dequeuedCell
 // This should be one function in one extension
 // but it doesn't seem possible to express.
-public extension dequeueReusableCellWithReuseIdentifier where Self: UICollectionView {
+public extension dequeueReusableCell where Self: UICollectionView {
 	///- Returns: A reusable cell dequeued without the standard need
 	///  for passing its name as a `String`,
 	///  and then casting to the desired type
 	///
 	///- Precondition: The name of `Cell` has been assigned to the `Identifier`
 	///  for a cell that this Collection View uses
-	final func cell
+	final func dequeuedCell
 	<Cell: UICollectionViewCell>
-	(indexPath indexPath: NSIndexPath) -> Cell {
-		return dequeueReusableCellWithReuseIdentifier_cell(indexPath: indexPath)
+	(indexPath: IndexPath) -> Cell {
+		return dequeueReusableCell_dequeuedCell(indexPath: indexPath)
 	}
 }
-public extension dequeueReusableCellWithReuseIdentifier where Self: UITableView {
+public extension dequeueReusableCell where Self: UITableView {
     ///- Returns: A reusable cell dequeued without the standard need
     ///  for passing its name as a `String`,
     ///  and then casting to the desired type
@@ -24,8 +24,8 @@ public extension dequeueReusableCellWithReuseIdentifier where Self: UITableView 
     final func cell
     <Cell: UIView>
     () -> Cell {
-        return dequeueReusableCellWithIdentifier(
-            String(Cell)
+        return dequeueReusableCell(
+            withIdentifier: String(Cell)
         ) as! Cell
     }
 
@@ -37,15 +37,15 @@ public extension dequeueReusableCellWithReuseIdentifier where Self: UITableView 
 	///  for a cell that this Table View uses
 	final func cell
 	<Cell: UITableViewCell>
-	(indexPath indexPath: NSIndexPath) -> Cell {
+	(indexPath: IndexPath) -> Cell {
 		return dequeueReusableCellWithReuseIdentifier_cell(indexPath: indexPath)
 	}
 }
-private extension dequeueReusableCellWithReuseIdentifier {
-	final func dequeueReusableCellWithReuseIdentifier_cell
+private extension dequeueReusableCell {
+	final func dequeueReusableCell_dequeuedCell
 	<Cell: UIView>
-	(indexPath indexPath: NSIndexPath) -> Cell {
-		return dequeueReusableCellWithReuseIdentifier(
+	(indexPath: IndexPath) -> Cell {
+		return dequeueReusableCell(
 			String(Cell),
 			forIndexPath: indexPath
 		) as! Cell
@@ -53,26 +53,33 @@ private extension dequeueReusableCellWithReuseIdentifier {
 }
 
 //MARK:- dequeueReusableCellWithReuseIdentifier
-public protocol dequeueReusableCellWithReuseIdentifier {
+public protocol dequeueReusableCell {
 	associatedtype Cell: UIView
-
+	
 	///- Attention: Called dequeueReusableCellWithIdentifier in UITableView
-	func dequeueReusableCellWithReuseIdentifier(
-		identifier: String,
-		forIndexPath indexPath: NSIndexPath
+	func dequeueReusableCell(
+		withReuseIdentifier _: String,
+		for _: IndexPath
 	) -> Cell
 }
 
-extension UICollectionView: dequeueReusableCellWithReuseIdentifier {}
+extension UICollectionView: dequeueReusableCell {}
 
-extension UITableView: dequeueReusableCellWithReuseIdentifier {
-	public final func dequeueReusableCellWithReuseIdentifier(
+extension UITableView: dequeueReusableCell {
+	public final func dequeueReusableCell(
 		identifier: String,
-		forIndexPath indexPath: NSIndexPath
+		forIndexPath indexPath: IndexPath
 	) -> UITableViewCell {
-		return dequeueReusableCellWithIdentifier(
-			identifier,
-			forIndexPath: indexPath
+		return dequeueReusableCell(
+			withIdentifier: identifier,
+			for: indexPath
 		)
+	}
+	
+	public func dequeueReusableCell(
+		withReuseIdentifier _: String,
+		for: IndexPath
+	) -> UITableViewCell {
+		return dequeueReusableCell(withIdentifier: "", for: f)
 	}
 }
