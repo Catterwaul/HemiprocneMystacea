a42c31c1c975094a443c1bc49f388daee48c2ebd
Jessy Catterwaul <mr.jessy@gmail.com>
2016-06-19 15:01:20 -0400

    nillifyUponCall

--- HemiprocneMystacea/Swift/nil closures.swift
+++ HemiprocneMystacea/Swift/nil closures.swift
@@ -2,41 +2,19 @@
 ///
 ///- Note: *Usage will look like this:*
 /// ```
-/// closure = nilledUponCall(closure){closure = $0}
-public func nilledUponCall<In, Out>(
-   _ closure: ((In) -> Out)!,
-   _ closure_set: (((In) -> Out)!) -> ()
-) -> ((In) -> Out)! {
-   // This stops recursive setting when used in property observers.
-   guard let _closure = closure else {return nil}
-   
-   var closure: ((In) -> Out)! {
-      get {return _closure}
-      set {closure_set(newValue)}
-   }
-   return {
-      defer {closure = nil}
-      return closure($0)
-   }
-}
-
-///- Parameter closure.set: setter for the closure
-///
-///- Note: *Usage will look like this:*
-///```
-/// nillifyUponCall(closure){closure = $0}`
-///```
-///- Important: You can't use this in a didSet;
-///  that would result in infinite recursion.
-public func nillifyUponCall<In, Out>(
-   _ closure: (In) -> Out,
-   closure_set: (((In) -> Out)!) -> ()
-) {
-   var closure: ((In) -> Out)! {
-      get {return closure}
-      set {closure_set(newValue)}
-   }
-   closure = nilledUponCall(closure, closure_set)
+/// closure = nillifyUponCall(closure){closure = $0}
+public typealias nillifyUponCall_Closure<Input, Output> = (Input) -> Output
+public func nillifyUponCall
+<Input, Output>(
+   _ closure: nillifyUponCall_Closure<Input, Output>?,
+   _ closure_set: (nillifyUponCall_Closure<Input, Output>?) -> ()
+) -> nillifyUponCall_Closure<Input, Output>? {
+	return closure ?… {closure in
+		{input in
+			defer {closure_set(nil)}
+			return closure(input)
+		}
+	}
 }
 
 //MARK:- nils
