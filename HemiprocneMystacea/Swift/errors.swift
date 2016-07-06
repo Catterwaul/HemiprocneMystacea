703ee7416aa2d42f7b40bf3b60bd18e7a4e386be
Jessy Catterwaul <mr.jessy@gmail.com>
2016-07-05 23:34:09 -0400

    Errors & Validate

---
+++ HemiprocneMystacea/Swift/errors.swift
@@ -0,0 +1,45 @@
+public typealias HashableError = protocol<ErrorProtocol, Hashable>
+
+public struct Errors
+<Error: HashableError>
+: ErrorProtocol {
+	public init
+	<Errors: Sequence where Errors.Iterator.Element == Error>
+	(_ errors: Errors) {
+		set = Set(errors)
+	}
+	public init(_ errors: Error...) {
+		self.init(errors)
+	}
+	
+	public let set: Set<Error>
+}
+
+
+public typealias Validate<Parameters> = (Parameters) throws -> Void
+
+public func validate<
+	Validates: Swift.Sequence,
+	Parameters,
+	Error: HashableError
+	where Validates.Iterator.Element == Validate<Parameters>
+>(
+	_ validates: Validates,
+	parameters: Parameters,
+	errorType: Error.Type
+) throws {
+	let errors = validates.flatMap{validate -> Set<Error> in
+		do {
+			try validate(parameters)
+			return []
+		} catch let error as Error {
+			return [error]
+		} catch let errors as Errors<Error> {
+			return errors.set
+		} catch {
+			fatalError()
+		}
+	}
+	guard errors.isEmpty
+	else {throw Errors(errors)}
+}
---
+++ HemiprocneMystaceaTests/Swift/errors_TestCase.swift
@@ -0,0 +1,37 @@
+import HemiprocneMystacea
+import XCTest
+
+final class errors_TestCase: XCTestCase {
+	func test() {
+		XCTAssertThrowsError(
+			try validate(
+				[throwBad, throwsStrongBads],
+				parameters: false,
+				errorType: Error.self
+			)
+		){error in
+			guard case let errors as Errors<Error> = error
+			else {
+				XCTFail()
+				return
+			}
+			
+			XCTAssertEqual(
+				errors.set,
+				[Error.bad, Error.strongBad]
+			)
+		}
+	}
+}
+
+private enum Error: ErrorProtocol {
+	case bad, strongBad
+}
+
+private func throwBad(_: Bool) throws {
+	throw Error.bad
+}
+
+private func throwsStrongBads(_: Bool) throws {
+	throw Errors(Error.strongBad, Error.strongBad)
+}
