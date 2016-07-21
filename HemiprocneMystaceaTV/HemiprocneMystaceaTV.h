50cb081643260e8ceaa8f779dcc6a9f8a37277f5
Catie Catterwaul <catiet@gmail.com>
2016-07-21 00:09:33 -0400

    Add tvOS target and shared scheme

---
+++ HemiprocneMystaceaTV/HemiprocneMystaceaTV.h
@@ -0,0 +1,19 @@
+//
+//  HemiprocneMystaceaTV.h
+//  HemiprocneMystaceaTV
+//
+//  Created by Catie Catterwaul on 7/21/16.
+//  Copyright © 2016 Catterwaul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HemiprocneMystaceaTV.
+FOUNDATION_EXPORT double HemiprocneMystaceaTVVersionNumber;
+
+//! Project version string for HemiprocneMystaceaTV.
+FOUNDATION_EXPORT const unsigned char HemiprocneMystaceaTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HemiprocneMystaceaTV/PublicHeader.h>
+
+
