42059aed27aabdff39a4d4530eb2733f97c38573
Jessy Catterwaul <mr.jessy@gmail.com>
2016-08-08 09:41:56 -0400

    UIImage.init?(asset: CKAsset)

---
+++ HemiprocneMystacea/CloudKit/CKAsset.swift
@@ -0,0 +1,14 @@
+import CloudKit
+import UIKit
+
+public extension UIImage {
+	convenience init?(asset: CKAsset) {
+		do {
+			let data = try Data(contentsOf: asset.fileURL)
+			self.init(data: data)
+		}
+		catch {
+			return nil
+		}
+	}
+}
