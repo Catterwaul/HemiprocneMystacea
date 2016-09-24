79805b1e9753d3d2ac6c1dc0d2fe9c9b430e193e
Jessy Catterwaul <mr.jessy@gmail.com>
2016-09-24 13:29:55 -0400

    TuplePlaceholder

--- HemiprocneMystacea/Swift/typealiases.swift
+++ HemiprocneMystacea/Swift/typealiases.swift
@@ -1,3 +1,5 @@
+public typealias TuplePlaceholder = Void
+
 public typealias Process<Processable> = (Processable) -> Void
 public typealias ProcessThrowingGet<Gettable> = Process<() throws -> Gettable>
 public typealias Verify = ProcessThrowingGet<Void>
