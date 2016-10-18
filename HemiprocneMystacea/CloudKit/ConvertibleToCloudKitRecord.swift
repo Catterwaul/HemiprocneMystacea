5a911e467244ef2018ec6c64f20a1c77f02384e0
Jessy Catterwaul <mr.jessy@gmail.com>
2016-10-18 08:33:41 -0400

    ConvertibleToCloudKitRecord

---
+++ HemiprocneMystacea/CloudKit/ConvertibleToCloudKitRecord.swift
@@ -0,0 +1,38 @@
+import CloudKit
+
+public protocol ConvertibleToCloudKitRecord {
+	///- Important: This is a nested type with this signature:
+	///  `enum CloudKitRecordKey: String`
+	associatedtype CloudKitRecordKey: RawRepresentable
+}
+
+public extension CKRecord {
+	/// - important: Type name of ConvertibleToCloudKitRecord is the name of its CKRecord
+	convenience init<ConvertibleToCloudKitRecord: HM.ConvertibleToCloudKitRecord>(
+		_ convertibleToCloudKitRecord: ConvertibleToCloudKitRecord
+	)
+	where ConvertibleToCloudKitRecord.CloudKitRecordKey.RawValue == String {
+		self.init(
+			recordType: String(describing: ConvertibleToCloudKitRecord.self)
+		)
+
+		Mirror(reflecting: convertibleToCloudKitRecord).children.flatMap{
+			label, value in
+			
+			guard
+				let label = label,
+				ConvertibleToCloudKitRecord.CloudKitRecordKey.contains(label),
+				let value = value as? CKRecordValue
+			else {return nil}
+			
+			return (
+				key: label,
+				value: value
+			)
+		}.forEach{
+			(	key: String,
+				value: CKRecordValue
+			) in self[key] = value
+		}
+	}
+}
