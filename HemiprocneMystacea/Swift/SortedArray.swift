b6292f9f424b38138154631e8a435d797bd726b1
Jessy Catterwaul <mr.jessy@gmail.com>
2017-02-10 09:41:32 -0500

    Delete now-unnecessary SortedArray code

--- HemiprocneMystacea/Swift/SortedArray.swift
+++ HemiprocneMystacea/Swift/SortedArray.swift
@@ -1,26 +1,11 @@
 public struct SortedArray<Element: Comparable>: BackedByArray {
 //MARK: BackedByArray
-	static func - (
-		backedByArray: SortedArray,
-		element: Element
-	) -> SortedArray {
-		return SortedArray(
-			backingArray: backedByArray.backingArray.filter{$0 != element}
-		)
-	}
-
 	public init<Elements: Sequence>(_ elements: Elements)
 	where Elements.Iterator.Element == Element {
 		backingArray = elements.sorted()
 	}
 
 	public fileprivate(set) var backingArray: [Element]
-	
-
-//MARK: private
-	private init(backingArray: [Element]) {
-		self.backingArray = backingArray
-	}
 }
 
 
