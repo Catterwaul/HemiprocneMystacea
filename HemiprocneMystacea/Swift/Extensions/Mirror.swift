883b09dc0a83a4ec8b4ccbb925f63af3b6714190
Jessy Catterwaul <mr.jessy@gmail.com>
2017-02-13 11:53:11 -0500

    Use a ? instead of .some

--- HemiprocneMystacea/Swift/Extensions/Mirror.swift
+++ HemiprocneMystacea/Swift/Extensions/Mirror.swift
@@ -1,7 +1,7 @@
 public extension Mirror {
 	var reflectsOptionalNone: Bool {
 		switch displayStyle {
-		case .some(.optional): return children.isEmpty
+		case .optional?: return children.isEmpty
 		default: return false
 		}
 	}
