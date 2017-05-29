db8e86a6b02ccc403958ffafc11893c04e1616d5
Jessy Catterwaul <mr.jessy@gmail.com>
2017-05-29 18:45:06 -0400

    Ints.joined

--- HemiprocneMystacea/Swift/Extensions/integers.swift
+++ HemiprocneMystacea/Swift/Extensions/integers.swift
@@ -1,4 +1,5 @@
-public extension ExpressibleByIntegerLiteral where Self: IntegerArithmetic {
+public extension ExpressibleByIntegerLiteral
+where Self: IntegerArithmetic {
 	/// *Divisbility*: when division by `divisor` results in a whole number.
 	func divisible(by divisor: Self) -> Bool {
 		return self % divisor == 0
@@ -10,4 +11,16 @@ where Iterator.Element: ExpressibleByIntegerLiteral & IntegerArithmetic {
 	var sum: Iterator.Element {
 		return self.reduce(0, +)
 	}
+}
+
+public extension Sequence
+where Iterator.Element == Int {
+  func joined(radix: Int = 10) -> Int? {
+    return Int(
+      self
+        .map{String($0, radix: radix)}
+        .joined(),
+      radix: radix
+    )
+  }
 }
--- HemiprocneMystaceaTests/Swift/Extensions/IntegersTestCase.swift
+++ HemiprocneMystaceaTests/Swift/Extensions/IntegersTestCase.swift
@@ -10,4 +10,21 @@ final class IntegersTestCase: XCTestCase {
 			33.divisible(by: 8)
 		)
 	}
+  
+  func test_joined() {
+    XCTAssertEqual(
+      [1, 2].joined(),
+      12
+    )
+    
+    XCTAssertEqual(
+      [1, 0b10].joined(radix: 0b10),
+      0b110
+    )
+    
+    XCTAssertEqual(
+      [1, 0x10].joined(radix: 0x10),
+      0x110
+    )
+  }
 }
