b82f726a435bd6383ac008acf2f5b127d4ec0105
Jessy Catterwaul <mr.jessy@gmail.com>
2017-06-15 02:40:55 -0400

    This note doesn't work in practice. Too much Objective-C going on. 😿

--- HemiprocneMystacea/QuartzCore/CABasicAnimation/CABasicAnimation.Values.swift
+++ HemiprocneMystacea/QuartzCore/CABasicAnimation/CABasicAnimation.Values.swift
@@ -1,8 +1,6 @@
 import QuartzCore
 
 public extension CABasicAnimation {
-	/// Should be `Values<Interpolable>`.
-	/// Change that for Swift 3.1!
 	public struct Values {
 		public init<Interpolable>(
 			from: Interpolable,
