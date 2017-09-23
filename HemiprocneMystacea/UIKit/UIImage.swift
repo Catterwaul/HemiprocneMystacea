8d8fd89437ef608d458814b32599ce1faeeac81c
Jessy Catterwaul <mr.jessy@gmail.com>
2017-09-23 06:45:27 -0400

    Add  KeyedEncodingContainer extensions for UIImage. They still need to be tested!

--- HemiprocneMystacea/UIKit/UIImage.swift
+++ HemiprocneMystacea/UIKit/UIImage.swift
@@ -9,3 +9,36 @@ public extension UIImage {
 		return getData(self) == getData(image)
 	}
 }
+
+public extension KeyedDecodingContainer {
+  func decode(key: Key) throws -> UIImage {
+    guard let image = UIImage( data: try decode(Data.self, forKey: key) )
+    else {
+      throw DecodingError.dataCorrupted(
+        DecodingError.Context(
+          codingPath: [key],
+          debugDescription: "Data does not represent a 'UIImage'"
+        )
+      )
+    }
+    
+    return image
+  }
+}
+
+public extension KeyedEncodingContainer {
+  mutating func encode(_ image: UIImage, forKey key: Key) throws {
+    guard let data = UIImagePNGRepresentation(image)
+    else {
+      throw EncodingError.invalidValue(
+        image,
+        EncodingError.Context(
+          codingPath: [key],
+          debugDescription: "Image cannot be represented with PNG data"
+        )
+      )
+    }
+    
+    try encode(data, forKey: key)
+  }
+}
