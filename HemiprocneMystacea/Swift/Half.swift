ab87795e42211e934d49af3e5255746585cdc870
Jessy Catterwaul <mr.jessy@gmail.com>
2018-06-18 22:35:49 -0400

    Half

---
+++ HemiprocneMystacea/Swift/Half.swift
@@ -0,0 +1,91 @@
+import Foundation
+
+// This should fully conform to `BinaryFloatingPoint`
+// but it would be a lot of work to implement that.
+public struct Half: Hashable {
+  public let bitPattern: UInt16
+}
+
+public extension Half {
+  static let exponentBitCount = 5
+  static let significandBitCount = 10
+  
+  init<Float: BinaryFloatingPoint>(_ float: Float) {
+    let shiftedExponentBit = (
+      float.sign == .plus
+      ? 0
+      : 1
+    ) << (Half.exponentBitCount + Half.significandBitCount)
+    
+    let shiftedExponentBitPattern =
+      Int(float.exponent + 0xF)
+      << Half.significandBitCount
+    
+    let significandBitPattern =
+      Int(float.significandBitPattern)
+      >> (Float.significandBitCount - Half.significandBitCount)
+    
+    bitPattern = UInt16(
+      shiftedExponentBit | shiftedExponentBitPattern | significandBitPattern
+    )
+  }
+  
+  var exponent: Int {
+    return exponentBitPattern - 0xF
+  }
+  
+  var exponentBitPattern: Int {
+    return
+      (Int(bitPattern) >> Half.significandBitCount)
+      & 0x1F
+  }
+  
+  var sign: FloatingPointSign {
+    return
+      bitPattern.leadingZeroBitCount > 0
+      ? .plus
+      : .minus
+  }
+  
+  var significandBitPattern: Int {
+    return Int(bitPattern) & 0x3FF
+  }
+}
+
+// MARK: Comparable
+extension Half: Comparable {
+  public static func < (half0: Half, half1: Half) -> Bool {
+    return Float(half0) < Float(half1)
+  }
+}
+
+// MARK: ExpressibleByFloatLiteral
+extension Half: ExpressibleByFloatLiteral {
+  public init(floatLiteral float: Float) {
+    self.init(float)
+  }
+}
+
+// MARK: ExpressibleByIntegerLiteral
+extension Half: ExpressibleByIntegerLiteral {
+  public init(integerLiteral int32: Int32) {
+    self.init( Float(int32) )
+  }
+}
+
+// MARK:-
+public extension BinaryFloatingPoint {
+  init(_ half: Half) {
+    self.init(
+      sign: half.sign,
+      exponentBitPattern: RawExponent(
+        half.exponentBitPattern
+        + Self.exponentBias - Half.exponentBias
+      ),
+      significandBitPattern: RawSignificand(
+        half.significandBitPattern
+        << (Self.significandBitCount - Half.significandBitCount)
+      )
+    )
+  }
+}
---
+++ HemiprocneMystaceaTests/Swift/Half.swift
@@ -0,0 +1,40 @@
+import HM
+import XCTest
+
+final class HalfTestCase: XCTestCase {
+  func test_size() {
+    XCTAssertEqual(MemoryLayout<Half>.size, 2)
+  }
+  
+  func test_exponent() {
+    XCTAssertEqual(9.7.exponent, (9.7 as Half).exponent)
+  }
+  
+  func test_Equatable() {
+    XCTAssertEqual(0 as Half, 0 as Half)
+    XCTAssertNotEqual(1 as Half, 2 as Half)
+  }
+  
+  func test_Comparable() {
+    XCTAssertLessThan(-20, 30)
+    XCTAssertLessThan(-20 as Half, 30 as Half)
+    
+    XCTAssertGreaterThan(1000, 8)
+    XCTAssertGreaterThan(1000 as Half, 8 as Half)
+  }
+  
+  func test_Conversion() {
+    XCTAssertEqual(
+      Float( 8 as Half ),
+      8.0
+    )
+    XCTAssertEqual(
+      Double( 777 as Half ),
+      777
+    )
+    XCTAssertEqual(
+      Float( -555.0 as Half ),
+      -555
+    )
+  }
+}
