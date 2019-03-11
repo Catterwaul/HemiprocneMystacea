eaf1391d735f851b4b3ce7b5ae8da2c611400825
Jessy Catterwaul <mr.jessy@gmail.com>
2019-03-11 12:40:01 -0400

    NibSuperview

--- HemiprocneMystacea/UIKit/UINib.swift
+++ HemiprocneMystacea/UIKit/UINib.swift
@@ -8,3 +8,42 @@ public extension UINib {
       .first { $0 is Object } as? Object
   }
 }
+
+
+/// A workaround for not being able to use a generic `UINib.Superview<Subview: UIView>` due to Objective-C.
+///
+/// `addSubview` is included in an extension. Suggested usage:
+///
+///      @IBDesignable final class Superview: UIView, NibSuperview {
+///        var subview: Subview!
+///
+///        override func awakeFromNib() {
+///          super.awakeFromNib()
+///          subview = addSubview()
+///        }
+///
+///        override func prepareForInterfaceBuilder() {
+///          super.prepareForInterfaceBuilder()
+///          subview = addSubview()
+///        }
+///      }
+protocol NibSuperview where Self: UIView {
+  associatedtype Subview: UIView
+
+  var subview: Subview! { get }
+}
+
+extension NibSuperview {
+  func addSubview() -> Subview {
+    let subview: Subview = UINib.instantiate()!
+    subview.translatesAutoresizingMaskIntoConstraints = false
+    addSubview(subview)
+    NSLayoutConstraint.activate([
+      subview.centerXAnchor.constraint(equalTo: centerXAnchor),
+      subview.centerYAnchor.constraint(equalTo: centerYAnchor),
+      subview.widthAnchor.constraint(equalTo: widthAnchor),
+      subview.heightAnchor.constraint(equalTo: heightAnchor)
+    ])
+    return subview
+  }
+}
