90a8d3f0300e0148d3b8f33be1ab6146c98c0a30
Jessy Catterwaul <mr.jessy@gmail.com>
2020-02-19 21:53:18 -0500

    EnumeratedSequence.Error

--- Sources/HM/Swift/extension/Collection/Lazy Views/EnumeratedSequence.swift
+++ Sources/HM/Swift/extension/Collection/Lazy Views/EnumeratedSequence.swift
@@ -1,4 +1,9 @@
 public extension EnumeratedSequence {
+  struct Error: Swift.Error {
+    public let index: Int
+    public let error: Swift.Error
+  }
+
   /// - Throws: EnumeratedSequenceError
   func mapElements<Transformed>(
     _ transform: (Base.Element) throws -> Transformed
@@ -6,16 +11,11 @@ public extension EnumeratedSequence {
     try self.map {
       do { return try transform($0.element) }
       catch {
-        throw EnumeratedSequenceError(
+        throw Error(
           index: $0.offset,
           error: error
         )
       }
     }
   }
-}
-
-public struct EnumeratedSequenceError: Error {
-  public let index: Int
-  public let error: Error
 }
--- Tests/HM/Swift/extension/Collection/Lazy Views/EnumeratedSequenceTestCase.swift
+++ Tests/HM/Swift/extension/Collection/Lazy Views/EnumeratedSequenceTestCase.swift
@@ -2,21 +2,20 @@ import HM
 import XCTest
 
 final class EnumeratedSequenceTestCase: XCTestCase {
-	func test_mapElements() throws {
-		do {
-			_ = try
-				["🚽", "🛁"]
-				.enumerated()
-				.mapElements{
-					guard $0 == "🚽"
-					else {
-						struct Error: Swift.Error { }
-						throw Error()
-					}
-				}
-		}
-		catch let error as EnumeratedSequenceError {
-			XCTAssertEqual(error.index, 1)
-		}
-	}
+  func test_mapElements() throws {
+    do {
+      _ = try
+        ["🚽", "🛁"]
+        .enumerated()
+        .mapElements {
+          guard $0 == "🚽" else {
+            struct Error: Swift.Error { }
+            throw Error()
+          }
+        }
+    }
+    catch let error as EnumeratedSequence<[String]>.Error {
+      XCTAssertEqual(error.index, 1)
+    }
+  }
 }
