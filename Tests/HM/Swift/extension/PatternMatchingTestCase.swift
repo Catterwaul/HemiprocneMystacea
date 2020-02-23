307718f645d036f2024d0b18146eea2c053ee00f
Jessy Catterwaul <mr.jessy@gmail.com>
2020-02-23 01:50:11 -0500

    Imrpove PatternMatchingTestCase

--- Tests/HM/Swift/extension/PatternMatchingTestCase.swift
+++ Tests/HM/Swift/extension/PatternMatchingTestCase.swift
@@ -2,25 +2,30 @@ import HM
 import XCTest
 
 final class PatternMatchingTestCase: XCTestCase {
-  func test_closure() {
-    switch 0.1 {
-    case
-      \.isZero,
-      [1, 2, 3].contains,
-      { $0 > 1 }
-    :
+  func test_Character() {
+    let g: Character = "g"
+    switch g {
+    case \.isNumber, \.isHexDigit:
       XCTFail()
-    default:
+    case \.isLetter:
       break
+    default:
+      XCTFail()
     }
   }
 
-  func test_KeyPath() {
-    switch "g" as Character {
-    case \.isNumber, \.isHexDigit:
+  func test_Double() {
+    switch 1.0 {
+    case
+      \.isZero,
+      [2, 100, -0.3].contains,
+      { $0 != 1 }
+    :
       XCTFail()
-    default:
+    case \.isInteger:
       break
+    default:
+      XCTFail()
     }
   }
 }
