33f9a21ca86c42d8341aba1ce6b1cfdf7ffad8c7
Jessy Catterwaul <mr.jessy@gmail.com>
2020-03-19 15:26:59 -0400

    Optional.reduce

--- Sources/HM/Swift/extension/Optional.swift
+++ Sources/HM/Swift/extension/Optional.swift
@@ -1,13 +1,11 @@
 public extension Optional {
-  /// If `self` is nil, `valueWhenNil` is assigned to `self`.
-  ///- Returns: The non-nil value of `self`
-  mutating func assignedIfNil(
-    _ getValueWhenNil: () -> Wrapped
-  ) -> Wrapped {
-    self
-    ?? {
-      self = getValueWhenNil()
-      return self!
-    } ()
+  /// - Parameters:
+  ///   - makeResult: arguments: (`resultWhenNil`, `self!`)
+  func reduce<Result>(
+    _ resultWhenNil: Result,
+    _ makeResult: (_ resultWhenNil: Result, _ self: Wrapped) throws -> Result
+  ) rethrows -> Result {
+    try self.map { try makeResult(resultWhenNil, $0) }
+    ?? resultWhenNil
   }
 }
--- Tests/HM/Swift/extension/OptionalTestCase.swift
+++ Tests/HM/Swift/extension/OptionalTestCase.swift
@@ -2,9 +2,26 @@ import HM
 import XCTest
 
 final class OptionalTestCase: XCTestCase {
-	func test_assignedIfNil() {
-		var bool: Bool? = nil
-		XCTAssertTrue( bool.assignedIfNil { true } )
-		XCTAssertTrue( bool.assignedIfNil { false } )
-	}
+  func test_reduce() {
+    var int: Int? = nil
+    XCTAssertEqual(int.reduce(1, +), 1)
+
+    int = 2
+    XCTAssertEqual(int.reduce(1, +), 3)
+
+
+    var optionalArray: [Int]? = nil
+    let array: [Int] = [1, 2]
+
+    XCTAssertEqual(
+      optionalArray.reduce(array) { $1 + $0 },
+      array
+    )
+
+    optionalArray = [0]
+    XCTAssertEqual(
+      optionalArray.reduce(array) { $1 + $0 },
+      [0, 1, 2]
+    )
+  }
 }
