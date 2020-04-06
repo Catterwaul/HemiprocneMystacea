d72c63702ccfa41d463840a5a2659e9aece052e5
Jessy Catterwaul <mr.jessy@gmail.com>
2020-04-06 00:08:40 -0400

    Comparable.getMax

--- Sources/HM/Swift/extension/Comparable.swift
+++ Sources/HM/Swift/extension/Comparable.swift
@@ -1,4 +1,41 @@
 public extension Comparable {
+  /// The properties with the maximum value.
+  /// - Parameter subject: An instance of any type.
+  static func getMax<Subject>(
+    _ subject: Subject
+  ) -> (labels: [String], value: Self)? {
+    getMax(subject, by: <)
+  }
+
+  /// The properties with the maximum value.
+  /// - Parameters:
+  ///   - subject: An instance of any type.
+  ///   - getAreInIncreasingOrder: Sorts two values.
+  static func getMax<Subject>(
+    _ subject: Subject,
+    by getAreInIncreasingOrder: (Self, Self) throws -> Bool
+  ) rethrows -> (labels: [String], value: Self)? {
+    let comparableChildren =
+      Mirror(reflecting: subject).children
+      .compactMap { child in
+        (child.value as? Self).map {
+          (label: child.label, value: $0)
+        }
+      }
+
+    guard let maxValue =
+      try comparableChildren.map(\.value)
+        .max(by: getAreInIncreasingOrder)
+    else { return nil }
+
+    return (
+      comparableChildren
+        .filter { $0.value == maxValue }
+        .compactMap(\.label),
+      maxValue
+    )
+  }
+
   func clamped(to limits: ClosedRange<Self>) -> Self {
     min( max(limits.lowerBound, self), limits.upperBound )
   }
--- Tests/HM/Swift/extension/ComparableTestCase.swift
+++ Tests/HM/Swift/extension/ComparableTestCase.swift
@@ -2,6 +2,35 @@ import HM
 import XCTest
 
 final class ComparableTestCase: XCTestCase {
+  func test_getMaxProperties() throws {
+    struct Subject {
+      let one = 1
+      let two = 2
+      let too = 2
+
+      let `false` = false
+    }
+
+    let maxIntProperties = try XCTUnwrap(
+      Int.getMax( Subject() )
+    )
+    XCTAssertEqual(maxIntProperties.labels, ["two", "too"])
+    XCTAssertEqual(maxIntProperties.value, 2)
+
+    XCTAssertNil(
+      Double.getMax( Subject() )
+    )
+
+    XCTAssertEqual(
+      Int.getMax( (1, 1) )?.labels,
+      [".0", ".1"]
+    )
+
+    XCTAssert(
+      Int.getMax([1])?.labels.isEmpty == true
+    )
+  }
+
   func test_clamped() {
     XCTAssertEqual("C".clamped(to: "F"..."P"), "F")
   }
