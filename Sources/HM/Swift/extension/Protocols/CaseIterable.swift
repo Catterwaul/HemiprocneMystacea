37c59485997d14ca45ad831ed2dfd369b46ff77e
Jessy Catterwaul <mr.jessy@gmail.com>
2020-04-08 01:31:48 -0400

    CaseIterable[case]

--- Sources/HM/Swift/extension/Protocols/CaseIterable.swift
+++ Sources/HM/Swift/extension/Protocols/CaseIterable.swift
@@ -1,4 +1,9 @@
 public extension CaseIterable where Self: Equatable {
+  /// The index of  a case, in `allCases`.
+  static subscript(_ case: Self) -> AllCases.Index {
+    allCases.firstIndex(of: `case`)!
+  }
+
   static func ...(minimum: Self, maximum: Self) -> AllCases.SubSequence {
     let indices = [minimum, maximum].compactMap(allCases.firstIndex)
     return allCases[ indices[0]...indices[1] ]
--- Tests/HM/Swift/extension/Protocols/CaseIterableTestCase.swift
+++ Tests/HM/Swift/extension/Protocols/CaseIterableTestCase.swift
@@ -2,6 +2,14 @@ import HM
 import XCTest
 
 final class CaseIterableTestCase: XCTestCase {
+  func test_subscript_case() {
+    enum 🦇: CaseIterable {
+      case 🧛🏻, 🦹🏿, 🏏
+    }
+
+    XCTAssertEqual(🦇[.🦹🏿], 1)
+  }
+
   func test_getRange() {
     enum Alphabet: CaseIterable {
       case a, b, c, d, e, f, g
