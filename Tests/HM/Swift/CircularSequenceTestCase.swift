e300f0b1557f37cadd35040f0eebee2357df0223
Jessy Catterwaul <mr.jessy@gmail.com>
2020-04-19 21:16:38 -0400

    CircularSequence.iterator

--- Tests/HM/Swift/CircularSequenceTestCase.swift
+++ Tests/HM/Swift/CircularSequenceTestCase.swift
@@ -1,12 +1,18 @@
 import HM
 import XCTest
 
-final class CircularSequenceTestCAse: XCTestCase {
+final class CircularSequenceTestCase: XCTestCase {
   func test() {
     enum 🦇: CaseIterable { case 🧛🏻, 🦹🏿, 🏏 }
     XCTAssertEqual(
       Array( CircularSequence(🦇.allCases).prefix(5) ),
       [.🧛🏻, .🦹🏿, .🏏, .🧛🏻, .🦹🏿]
     )
+
+    var iterator = CircularSequence(🦇.allCases).makeIterator()
+    for _ in 1...(🦇.allCases.count * 2 + 1) {
+      _ = iterator.next()
+    }
+    XCTAssertEqual(iterator.next(), .🦹🏿)
   }
 }
