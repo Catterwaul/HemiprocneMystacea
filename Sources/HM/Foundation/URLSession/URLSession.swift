9a25db4ef8af0dafbe3dc09e3496ffbd5472fda1
Jessy Catterwaul <mr.jessy@gmail.com>
2020-05-22 19:08:07 -0400

    URLSessionDataTask.Result

--- Sources/HM/Foundation/URLSession/URLSession.swift
+++ Sources/HM/Foundation/URLSession/URLSession.swift
@@ -1,27 +1,38 @@
 import Foundation
 
 public extension URLSession {
-  typealias DataTaskResult<Response: URLResponse> =
-    Result<(data: Data?, response: Response), Error>
-
   func makeDataTask<Response: URLResponse>(
     request: URLRequest,
-    process: @escaping (DataTaskResult<Response>) -> Void
+    process: @escaping (URLSessionDataTask.Result<Response>) -> Void
   ) -> URLSessionDataTask {
-    dataTask(with: request) { data, response, error in
-      process(
-        try! Result(
-          success: (response as? Response).map { (data, $0) },
-          failure: error
-        )
-      )
+    dataTask(with: request) {
+      process( .init(data: $0, response: $1, error: $2) )
     }
   }
 
   func makeHTTPDataTask(
     request: URLRequest,
-    process: @escaping (DataTaskResult<HTTPURLResponse>) -> Void
+    process: @escaping (URLSessionDataTask.Result<HTTPURLResponse>) -> Void
   ) -> URLSessionDataTask {
     makeDataTask(request: request, process: process)
+  }
+}
+
+public extension URLSessionDataTask {
+  typealias Result<Response: URLResponse> =
+    Swift.Result<(data: Data?, response: Response), Error>
+}
+
+public extension URLSessionDataTask.Result {
+  /// - Throws: `CastError.impossible` if  `response` is not a `Response`.
+  init<Response: URLResponse>(
+    data: Data?, response: URLResponse?,
+    error: Failure?
+  )
+  where Success == (data: Data?, response: Response) {
+    try! self.init(
+      success: (response as? Response).map { (data, $0) },
+      failure: error
+    )
   }
 }
