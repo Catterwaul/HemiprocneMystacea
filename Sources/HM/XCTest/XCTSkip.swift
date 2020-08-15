2a939aef25af2a620a1351d6f2317777caabf98d
Jessy Catterwaul <mr.jessy@gmail.com>
2020-08-15 13:17:36 -0400

    XCTSkip.uponFailure

---
+++ Sources/HM/XCTest/XCTSkip.swift
@@ -0,0 +1,14 @@
+import struct XCTest.XCTSkip
+
+public extension XCTSkip {
+  /// Skip the rest of a test if an error is thrown.
+  /// - Throws: `XCTSkip` if `getSuccess` throws an error.
+  /// - Returns: A `Success`, otherwise.
+  /// - Note: Similar to `XCTUnwrap`.
+  static func uponFailure<Success>(
+    of getSuccess: @autoclosure () throws -> Success
+  ) throws -> Success {
+    do { return try getSuccess() }
+    catch { throw XCTSkip() }
+  }
+}
