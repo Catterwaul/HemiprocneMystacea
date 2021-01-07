15bf383405352147a60d126da93e45b38d86c31b
Jessy Catterwaul <mr.jessy@gmail.com>
2021-01-07 02:32:28 -0500

    FloatingPoint.rounded(toNearestMultipleOf:)

--- Tests/HM/Swift/extension/Math/Floating/FloatingPointTestCase.swift
+++ Tests/HM/Swift/extension/Math/Floating/FloatingPointTestCase.swift
@@ -6,4 +6,10 @@ final class FloatingPointTestCase: XCTestCase {
     XCTAssert(1.0.isInteger)
     XCTAssertFalse((1.1 as CGFloat).isInteger)
   }
+
+  func test_quantized() {
+    XCTAssertEqual(Float16(0.16).rounded(toNearestMultipleOf: 1), 0)
+    XCTAssertEqual(-4.9.rounded(toNearestMultipleOf: 2), -4)
+    XCTAssertEqual(Float.pi.rounded(toNearestMultipleOf: 3), 3)
+  }
 }
