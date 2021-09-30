7151c7038493f43132211cee3ed07bcca420ad1c
Jessy Catterwaul <mr.jessy@gmail.com>
2021-09-30 00:56:57 -0400

    CKContainer

--- Sources/HM/CloudKit/extension/CKContainer.swift
+++ Sources/HM/CloudKit/extension/CKContainer.swift
@@ -1,37 +1,10 @@
 import CloudKit
 
 public extension CKContainer {
-  /// `userDiscoverability` is the only `CKApplicationPermissions`
-  /// so we don't bother with any as an argument.
-  func requestApplicationPermissionStatus(
-    process: @escaping (Result<ApplicationPermissionStatus, Error>) -> Void
-  ) {
-    requestApplicationPermission(
-      .userDiscoverability,
-      completionHandler: Result.makeHandleCompletion(process)
-    )
-  }
-
-  func requestUserIdentity(process: @escaping (Result<CKUserIdentity, Error>) -> Void) {
-    requestApplicationPermissionStatus { [unowned self] statusResult in
-      switch statusResult {
-      case .success(let status) where status == .granted:
-        self.fetchUserRecordID(
-          completionHandler: Result.makeHandleCompletion(
-            processSuccess: { [unowned self] id in
-              self.discoverUserIdentity(
-                withUserRecordID: id,
-                completionHandler: Result.makeHandleCompletion(process)
-              )
-            },
-            processFailure: process
-          )
-        )
-      case .failure(let error):
-        process(.failure(error))
-      case .success:
-        break
-      }
+  /// `userIdentity(forUserRecordID: userRecordID())`!
+  var userIdentity: CKUserIdentity {
+    get async throws {
+      try await userIdentity(forUserRecordID: userRecordID())!
     }
   }
 }
