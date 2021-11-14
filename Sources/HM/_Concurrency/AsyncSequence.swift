42335f85e8de0840b028d5e038c28f677ac21f83
Jessy Catterwaul <mr.jessy@gmail.com>
2021-11-14 14:26:45 -0500

    Employ `rethrows` to simplify

--- Sources/HM/_Concurrency/AsyncSequence.swift
+++ Sources/HM/_Concurrency/AsyncSequence.swift
@@ -9,38 +9,19 @@ public extension AsyncSequence {
 // MARK: - Array
 
 public extension Array {
-  init<AsyncSequence: _Concurrency.AsyncSequence>(_ asyncSequence: AsyncSequence) async throws
+  init<AsyncSequence: _Concurrency.AsyncSequence>(_ asyncSequence: AsyncSequence) async rethrows
   where AsyncSequence.Element == Element {
     self = try await asyncSequence.reduce(into: []) { $0.append($1) }
-  }
-  
-  init(_ taskGroup: TaskGroup<Element>) async {
-    self = await taskGroup.reduce(into: []) { $0.append($1) }
   }
 }
 
 // MARK: - Sequence
 
-public extension Sequence {
-  func map<Transformed>(
-    priority: TaskPriority? = nil,
-    _ transform: @escaping (Element) async -> Transformed
-  ) async -> [Transformed] {
-    await withTaskGroup(of: Transformed.self) { group in
-      for element in self {
-        group.addTask(priority: priority) {
-          await transform(element)
-        }
-      }
-      
-      return await .init(group)
-    }
-  }
-  
+public extension Sequence {  
   func map<Transformed>(
     priority: TaskPriority? = nil,
     _ transform: @escaping (Element) async throws -> Transformed
-  ) async throws -> [Transformed] {
+  ) async rethrows -> [Transformed] {
     try await withThrowingTaskGroup(of: Transformed.self) { group in
       for element in self {
         group.addTask(priority: priority) {
