3edca8b3031a1229628ef8047eaca9e3e0079e91
Jessy Catterwaul <mr.jessy@gmail.com>
2021-11-14 14:18:28 -0500

    async Sequence.map

--- Sources/HM/_Concurrency/AsyncSequence.swift
+++ Sources/HM/_Concurrency/AsyncSequence.swift
@@ -1,7 +1,54 @@
 public extension AsyncSequence {
   var collected: [Element] {
     get async throws {
-      try await reduce(into: []) { $0.append($1) }
+      try await .init(self)
+    }
+  }
+}
+
+// MARK: - Array
+
+public extension Array {
+  init<AsyncSequence: _Concurrency.AsyncSequence>(_ asyncSequence: AsyncSequence) async throws
+  where AsyncSequence.Element == Element {
+    self = try await asyncSequence.reduce(into: []) { $0.append($1) }
+  }
+  
+  init(_ taskGroup: TaskGroup<Element>) async {
+    self = await taskGroup.reduce(into: []) { $0.append($1) }
+  }
+}
+
+// MARK: - Sequence
+
+public extension Sequence {
+  func map<Transformed>(
+    priority: TaskPriority? = nil,
+    _ transform: @escaping (Element) async -> Transformed
+  ) async -> [Transformed] {
+    await withTaskGroup(of: Transformed.self) { group in
+      for element in self {
+        group.addTask(priority: priority) {
+          await transform(element)
+        }
+      }
+      
+      return await .init(group)
+    }
+  }
+  
+  func map<Transformed>(
+    priority: TaskPriority? = nil,
+    _ transform: @escaping (Element) async throws -> Transformed
+  ) async throws -> [Transformed] {
+    try await withThrowingTaskGroup(of: Transformed.self) { group in
+      for element in self {
+        group.addTask(priority: priority) {
+          try await transform(element)
+        }
+      }
+      
+      return try await .init(group)
     }
   }
 }
--- Tests/HM/_Concurrency/AsyncSequenceTestCase.swift
+++ Tests/HM/_Concurrency/AsyncSequenceTestCase.swift
@@ -7,4 +7,22 @@ final class AsyncSequenceTestCase: XCTestCase {
     let collected = try await AnyAsyncSequence(array).collected
     XCTAssertEqual(collected, array)
   }
+  
+  func test_map() async throws {
+    let array = ["🔥"]
+
+    func element(_ element: String) async throws -> String {
+      element
+    }
+        
+    do {
+      let transformed = try await array.map(element)
+      XCTAssertEqual(transformed, array)
+    }
+    
+    do {
+      let transformed = await array.map { try! await element($0) }
+      XCTAssertEqual(transformed, array)
+    }
+  }
 }
