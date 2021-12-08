83a0055b3ecd414bf63897875ae2182c67bc8a9b
Jessy Catterwaul <mr.jessy@gmail.com>
2021-12-07 21:07:44 -0500

    OrderedDictionary(grouping:)

---
+++ Tests/HM/OrderedCollections/OrderedDictionaryTestCase.swift
@@ -0,0 +1,47 @@
+import struct Collections.OrderedDictionary
+import HM
+import XCTest
+
+final class OrderedDictionaryTestCase: XCTestCase {
+  func test_init_grouping_KeyValuePairs() {
+    let dictionary: OrderedDictionary = [
+      "🔑": [
+        "🐅",
+        "🐆",
+        "🐈"
+      ],
+      "🗝": [
+        "🦖",
+        "🦕"
+      ]
+    ]
+
+    XCTAssertEqual(
+      OrderedDictionary(
+        grouping: [
+          ("🔑", "🐅"),
+          ("🔑", "🐆"),
+          ("🔑", "🐈"),
+
+          ("🗝", "🦖"),
+          ("🗝", "🦕")
+        ]
+      ),
+      dictionary
+    )
+
+    XCTAssertEqual(
+      OrderedDictionary(
+        grouping: [
+          "🔑": "🐅",
+          "🔑": "🐆",
+          "🔑": "🐈",
+
+          "🗝": "🦖",
+          "🗝": "🦕"
+        ] as KeyValuePairs
+      ),
+      dictionary
+    )
+  }
+}
