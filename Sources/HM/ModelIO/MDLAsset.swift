e362ad8e190c26099d3648712838a8788f040018
Jessy Catterwaul <mr.jessy@gmail.com>
2022-03-08 00:57:42 -0500

    MDLAsset.children

---
+++ Sources/HM/ModelIO/MDLAsset.swift
@@ -0,0 +1,8 @@
+import ModelIO
+
+public extension MDLAsset {
+  /// `childObjects(of: Object.self) as! [Object]`
+  func children<Object: MDLObject>() -> [Object] {
+    childObjects(of: Object.self) as! [Object]
+  }
+}
