4b91daf714981676f325d04d6e3c85e6e555a936
Jessy Catterwaul <mr.jessy@gmail.com>
2022-03-29 00:19:30 -0400

    BidirectionalCollection.firstDuplicate

--- Sources/HM/Swift/extension/BidirectionalCollection.swift
+++ Sources/HM/Swift/extension/BidirectionalCollection.swift
@@ -1,8 +1,22 @@
+import Algorithms
+import OrderedCollections
+
 public extension BidirectionalCollection where Element: Equatable {
   /// A subsequence starting at the last occurrence of `element.`
   ///
   ///- Returns: nil if `element` isn't present.
   func suffix(from element: Element) -> SubSequence? {
     lastIndex(of: element).map(suffix)
+  }
+}
+
+public extension BidirectionalCollection where Element: Hashable {
+  var firstDuplicate: (index: Index, element: Element)? {
+    var set: Set<Element> = []
+    return indexed().reversed().reduce(into: nil) {
+      if !set.insert($1.element).inserted {
+        $0 = $1
+      }
+    }
   }
 }
--- Tests/HM/Swift/extension/Collection/BidirectionalCollectionTestCase.swift
+++ Tests/HM/Swift/extension/Collection/BidirectionalCollectionTestCase.swift
@@ -1,9 +1,17 @@
 import HM
+import Algorithms
 import XCTest
 
 final class BidirectionalCollectionTestCase: XCTestCase {
   func test_suffix() {
     XCTAssertEqual("chunky skunky".suffix(from: "s"), "skunky")
     XCTAssertNil("aaabbbccc".suffix(from: "z"))
+  }
+
+  func test_firstDuplicate() {
+    XCTAssertEqual(
+      chain(1...5, [4, 3, 0]).firstDuplicate?.element,
+      3
+    )
   }
 }
