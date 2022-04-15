a9622bf325fa0c085dd328d1f801553de2175511
Jessy Catterwaul <mr.jessy@gmail.com>
2022-04-15 04:18:13 -0400

    Shift OptionSet.startingFlagIndex down by 1

--- Sources/HM/Swift/extension/Misc/OptionSet.swift
+++ Sources/HM/Swift/extension/Misc/OptionSet.swift
@@ -6,90 +6,90 @@ public extension OptionSet {
 }
 
 public extension OptionSet where RawValue: FixedWidthInteger {
-	/// Provides two options.
-	///
-	///- Parameter startingFlagIndex: shifts 0b11 if > 1
-	static func makeOptions(startingFlagIndex: RawValue = 1) -> (
-		Self,
-		Self
-	) {
-    ( Self(flagIndex: startingFlagIndex),
-			Self(flagIndex: startingFlagIndex + 1)
-		)
-	}
-	
-	/// Provides three options.
-	///
-	///- Parameter startingFlagIndex: shifts 0b111 if > 1
-	static func makeOptions(startingFlagIndex: RawValue = 1) -> (
-		Self,
-		Self,
-		Self
-	) {
-    ( Self(flagIndex: startingFlagIndex),
-			Self(flagIndex: startingFlagIndex + 1),
-			Self(flagIndex: startingFlagIndex + 2)
-		)
-	}
-	
-	/// Provides four options.
-	///
-	///- Parameter startingFlagIndex: shifts 0b1111 if > 1
-	static func makeOptions(startingFlagIndex: RawValue = 1) -> (
-		Self,
-		Self,
-		Self,
-		Self
-	) {
-    ( Self(flagIndex: startingFlagIndex),
-			Self(flagIndex: startingFlagIndex + 1),
-			Self(flagIndex: startingFlagIndex + 2),
-			Self(flagIndex: startingFlagIndex + 3)
-		)
-	}
-	
-	/// Provides five options.
-	///
-	///- Parameter startingFlagIndex: shifts 0b1_1111 if > 1
-	static func makeOptions(startingFlagIndex: RawValue = 1) -> (
-		Self,
-		Self,
-		Self,
-		Self,
-		Self
-	) {
-    ( Self(flagIndex: startingFlagIndex),
-			Self(flagIndex: startingFlagIndex + 1),
-			Self(flagIndex: startingFlagIndex + 2),
-			Self(flagIndex: startingFlagIndex + 3),
-			Self(flagIndex: startingFlagIndex + 4)
-		)
-	}
-	
-	/// Provides six options.
-	///
-	///- Parameter startingFlagIndex: shifts 0b11_1111 if > 1
-	static func makeOptions(startingFlagIndex: RawValue = 1) -> (
-		Self,
-		Self,
-		Self,
-		Self,
-		Self,
-		Self
-	) {
-    ( Self(flagIndex: startingFlagIndex),
-			Self(flagIndex: startingFlagIndex + 1),
-			Self(flagIndex: startingFlagIndex + 2),
-			Self(flagIndex: startingFlagIndex + 3),
-			Self(flagIndex: startingFlagIndex + 4),
-			Self(flagIndex: startingFlagIndex + 5)
-		)
-	}
+  /// Provides two options.
+  ///
+  ///- Parameter startingFlagIndex: shifts 0b11 if > 0
+  static func makeOptions(startingFlagIndex: RawValue = 0) -> (
+    Self,
+    Self
+  ) {
+    ( .init(flagIndex: startingFlagIndex),
+      .init(flagIndex: startingFlagIndex + 1)
+    )
+  }
+  
+  /// Provides three options.
+  ///
+  ///- Parameter startingFlagIndex: shifts 0b111 if > 0
+  static func makeOptions(startingFlagIndex: RawValue = 0) -> (
+    Self,
+    Self,
+    Self
+  ) {
+    ( .init(flagIndex: startingFlagIndex),
+      .init(flagIndex: startingFlagIndex + 1),
+      .init(flagIndex: startingFlagIndex + 2)
+    )
+  }
+  
+  /// Provides four options.
+  ///
+  ///- Parameter startingFlagIndex: shifts 0b1111 if > 0
+  static func makeOptions(startingFlagIndex: RawValue = 0) -> (
+    Self,
+    Self,
+    Self,
+    Self
+  ) {
+    ( .init(flagIndex: startingFlagIndex),
+      .init(flagIndex: startingFlagIndex + 1),
+      .init(flagIndex: startingFlagIndex + 2),
+      .init(flagIndex: startingFlagIndex + 3)
+    )
+  }
+  
+  /// Provides five options.
+  ///
+  ///- Parameter startingFlagIndex: shifts 0b1_1111 if > 0
+  static func makeOptions(startingFlagIndex: RawValue = 0) -> (
+    Self,
+    Self,
+    Self,
+    Self,
+    Self
+  ) {
+    ( .init(flagIndex: startingFlagIndex),
+      .init(flagIndex: startingFlagIndex + 1),
+      .init(flagIndex: startingFlagIndex + 2),
+      .init(flagIndex: startingFlagIndex + 3),
+      .init(flagIndex: startingFlagIndex + 4)
+    )
+  }
+  
+  /// Provides six options.
+  ///
+  ///- Parameter startingFlagIndex: shifts 0b11_1111 if > 0
+  static func makeOptions(startingFlagIndex: RawValue = 0) -> (
+    Self,
+    Self,
+    Self,
+    Self,
+    Self,
+    Self
+  ) {
+    ( .init(flagIndex: startingFlagIndex),
+      .init(flagIndex: startingFlagIndex + 1),
+      .init(flagIndex: startingFlagIndex + 2),
+      .init(flagIndex: startingFlagIndex + 3),
+      .init(flagIndex: startingFlagIndex + 4),
+      .init(flagIndex: startingFlagIndex + 5)
+    )
+  }
 }
 
 // MARK: private
 private extension OptionSet where RawValue: BinaryInteger {
-	init(flagIndex: RawValue) {
-		self.init(rawValue: 1 << (flagIndex - 1))
-	}
+  init(flagIndex: RawValue) {
+    self.init(rawValue: 1 << flagIndex)
+  }
 }
--- Tests/HM/Swift/extension/Misc/OptionSetTestCase.swift
+++ Tests/HM/Swift/extension/Misc/OptionSetTestCase.swift
@@ -1,8 +1,55 @@
-#if !os(macOS)
 import HM
 import XCTest
 
 final class OptionSetTypeTestCase: XCTestCase {
+  func test_2Flags() {
+    let options: Options = [.option1, .option2]
+    XCTAssertEqual(options.rawValue, 0b11)
+  }
+  
+  func test_3Flags() {
+    let options: Options = [
+      .option3,
+      .option4,
+      .option5
+    ]
+    XCTAssertEqual(options.rawValue, 0b1_1100)
+  }
+  
+  func test_4Flags() {
+    let options: Options = [
+      .option6,
+      .option7,
+      .option8,
+      .option9
+    ]
+    XCTAssertEqual(options.rawValue, 0b1_1110_0000)
+  }
+  
+  func test_5Flags() {
+    let options: Options = [
+      .option10,
+      .option11,
+      .option12,
+      .option13,
+      .option14
+    ]
+    XCTAssertEqual(options.rawValue, 0b11_1110_0000_0000)
+  }
+  
+  func test_6Flags() {
+    let options: Options = [
+      .option15,
+      .option16,
+      .option17,
+      .option18,
+      .option19,
+      .option20
+    ]
+    XCTAssertEqual(options.rawValue, 0b1111_1100_0000_0000_0000)
+  }
+  
+#if !os(macOS)
   func testInitWithCompatibleOptionSet() {
     let view = UIView()
     
@@ -18,99 +65,54 @@ final class OptionSetTypeTestCase: XCTestCase {
       [.flexibleHeight, .flexibleLeftMargin, .flexibleRightMargin]
     )
   }
-  
-	func test_2Flags() {
-		let options: Options = [.option1, .option2]
-		XCTAssertEqual(options.rawValue, 0b11)
-	}
-	
-	func test_3Flags() {
-		let options: Options = [
-			.option3,
-			.option4,
-			.option5
-		]
-		XCTAssertEqual(options.rawValue, 0b1_1100)
-	}
-	
-	func test_4Flags() {
-		let options: Options = [
-			.option6,
-			.option7,
-			.option8,
-			.option9
-		]
-		XCTAssertEqual(options.rawValue, 0b1_1110_0000)
-	}
-	
-	func test_5Flags() {
-		let options: Options = [
-			.option10,
-			.option11,
-			.option12,
-			.option13,
-			.option14
-		]
-		XCTAssertEqual(options.rawValue, 0b11_1110_0000_0000)
-	}
-	
-	func test_6Flags() {
-		let options: Options = [
-			.option15,
-			.option16,
-			.option17,
-			.option18,
-			.option19,
-			.option20
-		]
-		XCTAssertEqual(options.rawValue, 0b1111_1100_0000_0000_0000)
-	}
+#endif
 }
 
 private struct Options: OptionSet {
-	init(rawValue: UInt) {
-		self.rawValue = rawValue
-	}
-	
-	let rawValue: UInt
-	
-	static let (option1, option2) = Options.makeOptions()
-	static let (
-		option3,
-		option4,
-		option5
-	) = Options.makeOptions(startingFlagIndex: 3)
-	static let (
-		option6,
-		option7,
-		option8,
-		option9
-	) = Options.makeOptions(startingFlagIndex: 6)
-	static let (
-		option10,
-		option11,
-		option12,
-		option13,
-		option14
-	) = Options.makeOptions(startingFlagIndex: 10)
-	static let (
-		option15,
-		option16,
-		option17,
-		option18,
-		option19,
-		option20
-	) = Options.makeOptions(startingFlagIndex: 15)
+  init(rawValue: UInt) {
+    self.rawValue = rawValue
+  }
+  
+  let rawValue: UInt
+  
+  static let (option1, option2) = Options.makeOptions()
+  static let (
+    option3,
+    option4,
+    option5
+  ) = Options.makeOptions(startingFlagIndex: 2)
+  static let (
+    option6,
+    option7,
+    option8,
+    option9
+  ) = Options.makeOptions(startingFlagIndex: 5)
+  static let (
+    option10,
+    option11,
+    option12,
+    option13,
+    option14
+  ) = Options.makeOptions(startingFlagIndex: 9)
+  static let (
+    option15,
+    option16,
+    option17,
+    option18,
+    option19,
+    option20
+  ) = Options.makeOptions(startingFlagIndex: 14)
 }
 
+#if !os(macOS)
 extension UIView {
   struct AutoresizingFlexibilities: OptionSet {
     let rawValue: UInt
   }
   
   var autoresizingFlexibilities: AutoresizingFlexibilities {
-    get { AutoresizingFlexibilities(autoresizingMask) }
-    set { autoresizingMask = UIView.AutoresizingMask(newValue) }
+    get { .init(autoresizingMask) }
+    set { autoresizingMask = .init(newValue) }
   }
 }
 
