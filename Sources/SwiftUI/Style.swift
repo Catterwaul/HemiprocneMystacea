f2803a27758fa30d48989204403c3353bf54ce36
Jessy Catterwaul <mr.jessy@gmail.com>
2022-07-07 12:16:48 -0400

    Style

---
+++ Sources/SwiftUI/Style.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+
+/// A type-erased "`Style`".
+public struct AnyStyle<Configuration> {
+  private let makeBody: (Configuration) -> AnyView
+}
+
+// MARK: - public
+public extension AnyStyle {
+  init(makeBody: @escaping (Configuration) -> some View) {
+    self.makeBody = { .init(makeBody($0)) }
+  }
+
+  func makeBody(configuration: Configuration) -> some View {
+    makeBody(configuration)
+  }
+}
+
+// MARK: - ButtonStyle
+extension AnyStyle: ButtonStyle where Configuration == ButtonStyleConfiguration {
+  public init(_ style: some ButtonStyle) {
+    self.init(makeBody: style.makeBody)
+  }
+}
+
+// MARK: - GroupBoxStyle
+extension AnyStyle: GroupBoxStyle where Configuration == GroupBoxStyleConfiguration {
+  public init(_ style: some GroupBoxStyle) {
+    self.init(makeBody: style.makeBody)
+  }
+}
+
+// MARK: - LabelStyle
+extension AnyStyle: LabelStyle where Configuration == LabelStyleConfiguration {
+  public init(_ style: some LabelStyle) {
+    self.init(makeBody: style.makeBody)
+  }
+}
