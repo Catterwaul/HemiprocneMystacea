2861e8541a957aefebcb455baa557c049352fa13
Jessy Catterwaul <mr.jessy@gmail.com>
2022-12-20 02:11:46 -0500

    Font.Weight

---
+++ Sources/SwiftUI/Font.Weight.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+// MARK: - CaseIterable
+extension Font.Weight: CaseIterable {
+  @available(macOS, deprecated: 14, message: "It's that time of year—check for new cases!")
+  public static var allCases: [Self] {
+    [ ultraLight,
+      thin,
+      light,
+      regular,
+      medium,
+      semibold,
+      bold,
+      heavy,
+      black
+    ]
+  }
+}
+
+// MARK: - CustomStringConvertible
+extension Font.Weight: CustomStringConvertible {
+  @available(macOS, deprecated: 14, message: "It's that time of year—check for new cases!")
+  public var description: String {
+    switch self {
+    case .ultraLight: return "ultraLight"
+    case .thin: return "thin"
+    case .light: return "light"
+    case .regular: return "regular"
+    case .medium: return "medium"
+    case .semibold: return "semibold"
+    case .bold: return "bold"
+    case .heavy: return "heavy"
+    case .black: return "black"
+    default: return "\(self)"
+    }
+  }
+}
