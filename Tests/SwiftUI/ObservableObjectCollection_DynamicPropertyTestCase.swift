6875508446600e4c646bf97c19ebee77ca10c30a
Jessy Catterwaul <mr.jessy@gmail.com>
2023-02-16 12:39:03 -0500

    Flesh out ObservableObjectCollection_DynamicPropertyTestCase

--- Tests/SwiftUI/ObservableObjectCollection_DynamicPropertyTestCase.swift
+++ Tests/SwiftUI/ObservableObjectCollection_DynamicPropertyTestCase.swift
@@ -1,16 +1,35 @@
 import Combine
 import SwiftUI
-import SwiftUI_HM
 import XCTest
 
 final class ObservableObjectCollection_DynamicPropertyTestCase: XCTestCase {
   @MainActor func test() {
     struct View {
-      final class Object: ObservableObject { }
+      final class Object: ObservableObject {
+        @Published var property = "🤵🏻‍♀️"
+        @Published.Object.Collection var array: [Object] = []
+      }
+
       @ObservedObject.Collection var observed: [Object] = []
-      @StateObject.Collection var state: [Object] = []
+      @StateObject.Collection var state: [Object] = [.init(), .init()]
     }
 
-    _ = View()
+    let view = View()
+    view.observed = [.init(), .init()]
+
+    var value: String? = nil
+    var cancellable = view.observed[0].objectWillChange.sink {
+      value = view.observed[0].property
+    }
+    _ = cancellable
+    view.observed[0].property = "🤵🏽‍♂️"
+    XCTAssertEqual(value, "🤵🏻‍♀️")
+
+    view.state[0].array = [.init()]
+    cancellable = view.state[0].objectWillChange.sink {
+      value = view.observed[0].property
+      XCTAssertEqual(value, "🤵🏽‍♂️")
+    }
+    view.state[0].array[0].property = "🤵"
   }
 }
