db479068abd51f888e99bf8d482ae58e9117850a
Jessy Catterwaul <mr.jessy@gmail.com>
2023-02-17 11:59:32 -0500

    Storage Converters for Codable

--- Sources/SwiftUI/Storage Converter/AppStorage.Converter.swift
+++ Sources/SwiftUI/Storage Converter/AppStorage.Converter.swift
@@ -1,3 +1,4 @@
+import Combine
 import SwiftUI
 
 public extension AppStorage {
@@ -33,6 +34,27 @@ public extension AppStorage {
       self.toStorage = toStorage
       self.fromStorage = fromStorage
     }
+  }
+}
+
+// MARK: - Data
+/// - Warning: Not a good match for tvOS, due to severely limited local storage allowance.
+public extension AppStorage<Data>.Converter where Converted: Codable {
+  init<Decoder: TopLevelDecoder, Encoder: TopLevelEncoder>(
+    wrappedValue: Converted,
+    _ key: String, store: UserDefaults? = nil,
+    decoder: Decoder = JSONDecoder(), encoder: Encoder = JSONEncoder()
+  )
+  where Decoder.Input == Data, Encoder.Output == Data {
+    func toStorage(_ converted: Converted) -> Value {
+      (try? encoder.encode(converted)) ?? .init()
+    }
+    
+    self.init(
+      storage: .init(wrappedValue: toStorage(wrappedValue), key, store: store),
+      fromStorage: { (try? decoder.decode(Converted.self, from: $0)) ?? wrappedValue },
+      toStorage: toStorage
+    )
   }
 }
 
--- Sources/SwiftUI/Storage Converter/SceneStorage.Converter.swift
+++ Sources/SwiftUI/Storage Converter/SceneStorage.Converter.swift
@@ -1,7 +1,8 @@
+import Combine
 import SwiftUI
 
 public extension SceneStorage {
-  /// A way to use `AppStorage` with more types,
+  /// A way to use `SceneStorage` with more types,
   /// via conversion to and from the limited supported types.
   @propertyWrapper struct Converter<Converted> {
     public typealias ToStorage = (Converted) -> Value
@@ -33,6 +34,27 @@ public extension SceneStorage {
       self.toStorage = toStorage
       self.fromStorage = fromStorage
     }
+  }
+}
+
+// MARK: - Data
+/// - Warning: Not a good match for tvOS, due to severely limited local storage allowance.
+public extension SceneStorage<Data>.Converter where Converted: Codable {
+  init<Decoder: TopLevelDecoder, Encoder: TopLevelEncoder>(
+    wrappedValue: Converted,
+    _ key: String, store: UserDefaults? = nil,
+    decoder: Decoder = JSONDecoder(), encoder: Encoder = JSONEncoder()
+  )
+  where Decoder.Input == Data, Encoder.Output == Data {
+    func toStorage(_ converted: Converted) -> Value {
+      (try? encoder.encode(converted)) ?? .init()
+    }
+
+    self.init(
+      storage: .init(wrappedValue: toStorage(wrappedValue), key),
+      fromStorage: { (try? decoder.decode(Converted.self, from: $0)) ?? wrappedValue },
+      toStorage: toStorage
+    )
   }
 }
 
