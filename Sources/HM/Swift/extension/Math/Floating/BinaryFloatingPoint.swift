36a6d5f0f6e9d4912d722de26ad5312379be5b28
Jessy Catterwaul <mr.jessy@gmail.com>
2024-05-07 01:35:07 -0400

    Remove Half

--- Sources/HM/Swift/extension/Math/Floating/BinaryFloatingPoint.swift
+++ Sources/HM/Swift/extension/Math/Floating/BinaryFloatingPoint.swift
@@ -8,11 +8,5 @@ protocol BinaryFloatingPoint_exponentBias {
   static var exponentBias: Int { get }
 }
 
-extension Half: BinaryFloatingPoint_exponentBias {
-  static var exponentBias: Int {
-    1 << (exponentBitCount - 1)
-  }
-}
-
 extension Double: BinaryFloatingPoint_exponentBias { }
 extension Float: BinaryFloatingPoint_exponentBias { }
